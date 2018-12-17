54d0d4e000fdbca0bb5b9b62247d0300714b1a27
Andrei Hodanenok <xoda91@gmail.com>
2018-12-17 20:35:07 +0300

    2018: day 9, part 1

---
+++ 2018/09/Day9.java
@@ -0,0 +1,108 @@
+import java.util.PriorityQueue;
+
+/**
+ * Advent of Code - Day 9
+ * https://adventofcode.com/2018/day/9
+ */
+public class Day9 {
+
+    public static void main(String[] args) {
+        part1(9, 25);
+        part1(10, 1618);
+        part1(13, 7999);
+        part1(17, 1104);
+        part1(21, 6111);
+        part1(30, 5807);
+        part1(459, 71790);
+    }
+
+    private static void part1(int playersCount, int marblesCount) {
+        Game g = new Game(playersCount, marblesCount);
+        while (!g.isOver()) {
+            g.nextTurn();
+        }
+
+        System.out.println(g.maxScore());
+    }
+
+    private static class Game {
+
+        int[] playersScore;
+        int currentPlayer;
+
+        Marble currentMarble;
+        int nextMarbleNum;
+        int marblesCount;
+
+        Game(int playersCount, int marblesCount) {
+            this.playersScore = new int[playersCount];
+            this.currentPlayer = 0;
+            this.nextMarbleNum = 1;
+            this.marblesCount = marblesCount;
+
+            this.currentMarble = new Marble(0);
+            this.currentMarble.left = this.currentMarble;
+            this.currentMarble.right = this.currentMarble;
+        }
+
+        boolean isOver() {
+            return nextMarbleNum > marblesCount;
+        }
+
+        int maxScore() {
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < playersScore.length; i++) {
+                max = Math.max(playersScore[i], max);
+            }
+
+            return max;
+        }
+
+        void nextTurn() {
+            int placedMarbleNum = nextMarbleNum;
+            nextMarbleNum++;
+
+            if (placedMarbleNum % 23 == 0) {
+                Marble removedMarble = currentMarble;
+                for (int i = 0; i < 7; i++) {
+                    removedMarble = removedMarble.left;
+                }
+                currentMarble = removedMarble.right;
+
+                playersScore[currentPlayer] += placedMarbleNum;
+                playersScore[currentPlayer] += removedMarble.num;
+
+                // removing marble from circle
+                removedMarble.left.right = removedMarble.right;
+                removedMarble.right.left = removedMarble.left;
+                removedMarble.left = null;
+                removedMarble.right = null;
+            } else {
+                Marble leftNeighbour = currentMarble.right;
+                Marble rightNeighbour = currentMarble.right.right;
+
+                Marble marble = new Marble(placedMarbleNum);
+                marble.left = leftNeighbour;
+                marble.right = rightNeighbour;
+
+                leftNeighbour.right = marble;
+                rightNeighbour.left = marble;
+
+                currentMarble = marble;
+            }
+
+            currentPlayer = (currentPlayer + 1) % playersScore.length;
+        }
+    }
+
+    private static class Marble {
+
+        final int num;
+        Marble left;
+        Marble right;
+
+        Marble(int num) {
+            this.num = num;
+        }
+    }
+}
