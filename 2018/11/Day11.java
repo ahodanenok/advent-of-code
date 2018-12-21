de1a26b8bc635ecdcafe5f1b9cdfac99a37be4f4
Andrei Hodanenok <xoda91@gmail.com>
2018-12-21 23:09:16 +0300

    2018: day 11, part 2

--- 2018/11/Day11.java
+++ 2018/11/Day11.java
@@ -5,27 +5,49 @@
 public class Day11 {
 
     public static void main(String[] args) {
-        part1(7803);
+        Grid grid = new Grid(300, 7803);
+        part1(grid);
+        part2(grid);
     }
 
-    private static void part1(int number) {
-        Grid grid = new Grid(300, number);
-
-        int sy = 0;
-        int sx = 0;
+    private static void part1(Grid grid) {
+        int topY = 0;
+        int topX = 0;
         int maxPower = Integer.MIN_VALUE;
         for (int y = 0; y < grid.getSize(); y++) {
             for (int x = 0; x < grid.getSize(); x++) {
-                int power = grid.getSquarePowerLevel(x, y);
+                int power = grid.getSquarePowerLevel(x, y, 3);
                 if (power > maxPower) {
-                    sy = y;
-                    sx = x;
+                    topX = x;
+                    topY = y;
                     maxPower = power;
                 }
             }
         }
 
-        System.out.println(sx + "," + sy);
+        System.out.println(topX + "," + topY);
+    }
+
+    private static void part2(Grid grid) {
+        int topY = 0;
+        int topX = 0;
+        int squareSize = 1;
+        int maxPower = Integer.MIN_VALUE;
+        for (int y = 0; y < grid.getSize(); y++) {
+            for (int x = 0; x < grid.getSize(); x++) {
+                for (int s = 1; s < grid.getSize(); s++) {
+                    int power = grid.getSquarePowerLevel(x, y, s);
+                    if (power > maxPower) {
+                        topX = x;
+                        topY = y;
+                        squareSize = s;
+                        maxPower = power;
+                    }
+                }
+            }
+        }
+
+        System.out.println(topX + "," + topY + "," + squareSize);
     }
 
     private static class Grid {
@@ -42,14 +64,14 @@ public class Day11 {
             return grid.length;
         }
 
-        int getSquarePowerLevel(int topX, int topY) {
-            if (topY + 3 >= getSize() || topX + 3 >= getSize()) {
+        int getSquarePowerLevel(int topX, int topY, int squareSize) {
+            if (topY + squareSize >= getSize() || topX + squareSize >= getSize()) {
                 return Integer.MIN_VALUE;
             }
 
             int power = 0;
-            for (int y = topY; y < topY + 3; y++) {
-                for (int x = topX; x < topX + 3; x++) {
+            for (int y = topY; y < topY + squareSize; y++) {
+                for (int x = topX; x < topX + squareSize; x++) {
                     power += getCellPowerLevel(x, y);
                 }
             }
