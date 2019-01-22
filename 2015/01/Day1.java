fbab9511a6f0cd28f60b265fb02148c91a11af50
Andrei Hodanenok <xoda91@gmail.com>
2019-01-22 20:01:24 +0300

    2015: day 1, part 2

--- 2015/01/Day1.java
+++ 2015/01/Day1.java
@@ -8,6 +8,7 @@ public class Day1 {
     public static void main(String[] args) {
         String steps = getSteps();
         part1(steps);
+        part2(steps);
     }
 
     private static void part1(String steps) {
@@ -24,6 +25,25 @@ public class Day1 {
         }
 
         System.out.println(floor);
+    }
+
+    private static void part2(String steps) {
+        int floor = 0;
+        int i = 0;
+        while (i < steps.length() && floor >= 0) {
+            char step = steps.charAt(i);
+            if (step == UP) {
+                floor++;
+            } else if (step == DOWN) {
+                floor--;
+            } else {
+                throw new IllegalArgumentException("Unknown step: " + step);
+            }
+
+            i++;
+        }
+
+        System.out.println(i);
     }
 
     private static String getSteps() {
