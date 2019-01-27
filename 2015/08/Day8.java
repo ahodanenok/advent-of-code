ca50ea59d4c9e11f20a5590a595dabee8c38e5e5
Andrei Hodanenok <xoda91@gmail.com>
2019-01-27 20:05:18 +0300

    2015: day 8, part 2

--- 2015/08/Day8.java
+++ 2015/08/Day8.java
@@ -11,6 +11,7 @@ public class Day8 {
     public static void main(String[] args) {
         List<String> strings = getStrings();
         part1(strings);
+        part2(strings);
     }
 
     private static void part1(List<String> strings) {
@@ -43,6 +44,27 @@ public class Day8 {
         }
 
         System.out.println(literalCharactersCount - memoryCharactersCount);
+    }
+
+    private static void part2(List<String> strings) {
+        int literalCharactersCount = 0;
+        int encodedCharactersCount = 0;
+
+        for (String str : strings) {
+            for (int pos = 0; pos < str.length(); pos++) {
+                char ch = str.charAt(pos);
+                if (ch == '"' || ch == '\\') {
+                    encodedCharactersCount += 2;
+                } else {
+                    encodedCharactersCount++;
+                }
+            }
+
+            encodedCharactersCount += 2;
+            literalCharactersCount += str.length();
+        }
+
+        System.out.println(encodedCharactersCount - literalCharactersCount);
     }
 
     private static List<String> getStrings() {
