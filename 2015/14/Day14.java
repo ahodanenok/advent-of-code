3b270e605b263d85a7c08c76dad9015958a85f90
Andrei Hodanenok <xoda91@gmail.com>
2019-02-01 00:06:14 +0300

    2015: day 14, part 2

--- 2015/14/Day14.java
+++ 2015/14/Day14.java
@@ -8,22 +8,61 @@ import java.util.ArrayList;
  */
 public class Day14 {
 
+    private static final int TIME = 2503;
+
     public static void main(String[] args) {
         List<Reindeer> deers = getReindeers();
         part1(deers);
+        part2(deers);
     }
 
     private static void part1(List<Reindeer> deers) {
-        int time = 2503;
         int maxDistance = Integer.MIN_VALUE;
         for (Reindeer deer : deers) {
             int cycleTime = deer.flyingTime + deer.restingTime;
-            int cyclesCount = time / cycleTime;
-            int totalFlyingTime = cyclesCount * deer.flyingTime + Math.min(time % cycleTime, deer.flyingTime);
+            int cyclesCount = TIME / cycleTime;
+            int totalFlyingTime = cyclesCount * deer.flyingTime + Math.min(TIME % cycleTime, deer.flyingTime);
             maxDistance = Math.max(deer.flyingSpeed * totalFlyingTime, maxDistance);
         }
 
         System.out.println(maxDistance);
+    }
+
+    private static void part2(List<Reindeer> deers) {
+        int[] points = new int[deers.size()];
+        int[] distance = new int[deers.size()];
+        int[] startAt = new int[deers.size()];
+
+        for (int t = 0; t < TIME; t++) {
+            for (int i = 0; i < deers.size(); i++) {
+                Reindeer deer = deers.get(i);
+                if (startAt[i] < t - deer.flyingTime + 1) {
+                    startAt[i] += deer.flyingTime + deer.restingTime;
+                }
+
+                if (t >= startAt[i]) {
+                    distance[i] += deer.flyingSpeed;
+                }
+            }
+
+            int maxDistance = Integer.MIN_VALUE;
+            for (int i = 0; i < distance.length; i++) {
+                maxDistance = Math.max(distance[i], maxDistance);
+            }
+
+            for (int i = 0; i < distance.length; i++) {
+                if (distance[i] == maxDistance) {
+                    points[i]++;
+                }
+            }
+        }
+
+        int maxPoints = Integer.MIN_VALUE;
+        for (int i = 0; i < points.length; i++) {
+            maxPoints = Math.max(points[i], maxPoints);
+        }
+
+        System.out.println(maxPoints);
     }
 
     private static List<Reindeer> getReindeers() {
@@ -44,9 +83,9 @@ public class Day14 {
 
     private static class Reindeer {
 
-        private int flyingSpeed;
-        private int flyingTime;
-        private int restingTime;
+        private final int flyingSpeed;
+        private final int flyingTime;
+        private final int restingTime;
 
         Reindeer(int flyingSpeed, int flyingTime, int restingTime) {
             this.flyingSpeed = flyingSpeed;
