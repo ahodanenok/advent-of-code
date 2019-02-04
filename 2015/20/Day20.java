192e5c8d4195866c966d37d7db2b5df6e53228c5
Andrei Hodanenok <xoda91@gmail.com>
2019-02-04 20:05:56 +0300

    2015: day 20, part 1

---
+++ 2015/20/Day20.java
@@ -0,0 +1,61 @@
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Advent of Code - Day 20
+ * https://adventofcode.com/2015/day/20
+ */
+public class Day20 {
+
+    public static void main(String[] args) {
+        int presentsCount = 29_000_000;
+        part1(presentsCount);
+
+        //System.out.println(visitedByElves(1));
+        //System.out.println(visitedByElves(2));
+        //System.out.println(visitedByElves(3));
+        //System.out.println(visitedByElves(4));
+        //System.out.println(visitedByElves(5));
+        //System.out.println(visitedByElves(6));
+        //System.out.println(visitedByElves(7));
+        //System.out.println(visitedByElves(8));
+        //System.out.println(visitedByElves(9));
+    }
+
+    private static void part1(int presentsCount) {
+        int houseNum = 1;
+        while (presentsByHouse(houseNum) < presentsCount) {
+            houseNum++;
+        }
+
+        System.out.println(houseNum);
+    }
+
+    private static List<Integer> visitedByElves(int houseNum) {
+        List<Integer> elves = new ArrayList<Integer>();
+        elves.add(1);
+
+        int elf = 2;
+        int stopAt = houseNum / 2;
+        while (elf <= stopAt) {
+            if (houseNum % elf == 0) {
+                elves.add(elf);
+            }
+
+            elf++;
+        }
+
+        elves.add(houseNum);
+        return elves;
+    }
+
+    private static int presentsByHouse(int houseNum) {
+        List<Integer> elves = visitedByElves(houseNum);
+        int presents = 0;
+        for (Integer elf : elves) {
+            presents += elf * 10;
+        }
+
+        return presents;
+    }
+}
