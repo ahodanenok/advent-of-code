18f037026f49776e9e573a0b766dfcbcd35ff63b
Andrei Hodanenok <xoda91@gmail.com>
2019-02-10 01:35:26 +0300

    2016: day 4, part 1

---
+++ 2016/04/Day4.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * Advent of Code - Day 4
+ * https://adventofcode.com/2016/day/4
+ */
+public class Day4 {
+
+    public static void main(String[] args) {
+        List<Room> rooms = getRooms();
+        part1(rooms);
+    }
+
+    private static void part1(List<Room> rooms) {
+        int sectorIdSum = 0;
+        for (Room r : rooms) {
+            if (r.isValid()) {
+                sectorIdSum += r.sectorId;
+            }
+        }
+
+        System.out.println(sectorIdSum);
+    }
+
+    private static List<Room> getRooms() {
+        List<Room> rooms = new ArrayList<Room>();
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            rooms.add(new Room(line));
+        }
+
+        return rooms;
+    }
+
+    private static class Room {
+
+        private final String encryptedName;
+        private final int sectorId;
+        private final String checksum;
+
+        Room(String name) {
+            int checksumStart = name.indexOf('[');
+            this.checksum = name.substring(checksumStart + 1, name.length() - 1);
+
+            int sectorSeparatorPos = name.lastIndexOf('-');
+            this.encryptedName = name.substring(0, sectorSeparatorPos);
+            this.sectorId = Integer.parseInt(name.substring(sectorSeparatorPos + 1, checksumStart));
+        }
+
+        boolean isValid() {
+            Map<Character, Integer> chars = new HashMap<Character, Integer>();
+            for (int i = 0; i < encryptedName.length(); i++) {
+                char ch = encryptedName.charAt(i);
+                if (!Character.isAlphabetic(ch)) {
+                    continue;
+                }
+
+                if (chars.containsKey(ch)) {
+                    chars.put(ch, chars.get(ch) + 1);
+                } else {
+                    chars.put(ch, 1);
+                }
+            }
+
+            List<Map.Entry<Character, Integer>> entries =
+                    new ArrayList<Map.Entry<Character, Integer>>(chars.entrySet());
+            Collections.sort(entries, new Comparator<Map.Entry<Character, Integer>>() {
+                public int compare(Map.Entry<Character, Integer> a, Map.Entry<Character, Integer> b) {
+                    if (a.getValue() > b.getValue()) {
+                        return -1;
+                    } else if (a.getValue() < b.getValue()) {
+                        return 1;
+                    } else {
+                        return Character.compare(a.getKey(), b.getKey());
+                    }
+                }
+            });
+
+            String generatedChecksum = "";
+            for (int i = 0; i < 5; i++) {
+                Map.Entry<Character, Integer> entry = entries.get(i);
+                generatedChecksum += entry.getKey();
+            }
+            //System.out.println("checksum=" + checksum + ", generatedChecksum=" + generatedChecksum);
+            return checksum.equals(generatedChecksum);
+        }
+    }
+}
