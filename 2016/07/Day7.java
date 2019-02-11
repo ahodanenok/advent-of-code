1bd62b44c1a5988de1ecc43c152bb14a954e4554
Andrei Hodanenok <xoda91@gmail.com>
2019-02-11 23:22:20 +0300

    2016: day 7, part 1

---
+++ 2016/07/Day7.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Advent of Code - Day 7
+ * https://adventofcode.com/2016/day/7
+ */
+public class Day7 {
+
+    public static void main(String[] args) {
+        List<IP7> addresses = getAddresses();
+        part1(addresses);
+    }
+
+    private static void part1(List<IP7> addresses) {
+        int count = 0;
+        for (IP7 ip7 : addresses) {
+            if (ip7.hasABBA()) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    private static List<IP7> getAddresses() {
+        List<IP7> addresses = new ArrayList<IP7>();
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            addresses.add(new IP7(scanner.nextLine().trim()));
+        }
+
+        return addresses;
+    }
+
+    private static class IP7 {
+
+        private final List<String> parts;
+        private final List<String> sequences;
+
+        IP7(String ip) {
+            this.parts = new ArrayList<String>();
+            this.sequences = new ArrayList<String>();
+
+            for (String str : ip.split("\\[")) {
+                int idx = str.indexOf(']');
+                if (idx > -1) {
+                    sequences.add(str.substring(0, idx));
+                    if (idx < str.length() - 1) {
+                        parts.add(str.substring(idx + 1));
+                    }
+                } else {
+                    parts.add(str);
+                }
+            }
+
+            //System.out.print(parts);
+            //System.out.print("   ");
+            //System.out.println(sequences);
+        }
+
+        boolean hasABBA() {
+            for (String seq : sequences) {
+                for (int i = 3; i < seq.length(); i++) {
+                    char a = seq.charAt(i - 3);
+                    char b = seq.charAt(i - 2);
+                    char c = seq.charAt(i - 1);
+                    char d = seq.charAt(i - 0);
+                    if (isABBA(a, b, c, d)) {
+                        return false;
+                    }
+                }
+            }
+
+            for (String part : parts) {
+                for (int i = 3; i < part.length(); i++) {
+                    char a = part.charAt(i - 3);
+                    char b = part.charAt(i - 2);
+                    char c = part.charAt(i - 1);
+                    char d = part.charAt(i - 0);
+                    if (isABBA(a, b, c, d)) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private boolean isABBA(char a, char b, char c, char d) {
+            return a != b && a == d && b == c;
+        }
+    }
+}
