86a420ee998e98586bf1f1019f1d307a72a6c15d
Andrei Hodanenok <xoda91@gmail.com>
2019-03-04 20:02:22 +0300

    2016: day 19, part 2

--- 2016/19/Day19.java
+++ 2016/19/Day19.java
@@ -8,6 +8,7 @@ public class Day19 {
 
     public static void main(String[] args) {
         part1(3004953);
+        part2(3004953);
     }
 
     private static void part1(int elfsCount) {
@@ -23,6 +24,38 @@ public class Day19 {
         while (n.next != n) {
             n.next = n.next.next;
             n = n.next;
+        }
+
+        System.out.println(n.idx + 1);
+    }
+
+    private static void part2(int elfsCount) {
+        Elf[] elfs = new Elf[elfsCount];
+        elfs[0] = new Elf(0);
+        for (int i = 1; i < elfs.length; i++) {
+            elfs[i] = new Elf(i);
+            elfs[i - 1].next = elfs[i];
+        }
+        elfs[elfs.length - 1].next = elfs[0];
+
+        int elfsLeft = elfs.length;
+        Elf n = elfs[0];
+        while (n.next != n) {
+            int skip = elfsLeft / 2;
+            Elf exch = n;
+            while (skip > 1) {
+                exch = exch.next;
+                skip--;
+            }
+
+            exch.next = exch.next.next;
+            n = n.next;
+            elfsLeft--;
+
+            //System.out.println(elfsLeft);
+            if (elfsLeft % 1000 == 0) {
+                System.out.println(elfsLeft);
+            }
         }
 
         System.out.println(n.idx + 1);
