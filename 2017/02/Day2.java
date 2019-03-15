337ebf08ece6a55ea3a5f34c043db6b9d8e0b11d
Andrei Hodanenok <xoda91@gmail.com>
2019-03-16 01:25:34 +0300

    2017: day 2, part 1

---
+++ 2017/02/Day2.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Advent of Code - Day 2
+ * https://adventofcode.com/2017/day/2
+ */
+public class Day2 {
+
+    public static void main(String[] args) {
+        List<List<Integer>> data = getData();
+        part1(data);
+    }
+
+    private static void part1(List<List<Integer>> data) {
+        int sum = 0;
+        for (List<Integer> row : data) {
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            for (int b : row) {
+                min = Math.min(b, min);
+                max = Math.max(b, max);
+            }
+
+            sum += max - min;
+        }
+
+        System.out.println(sum);
+    }
+
+    private static List<List<Integer>> getData() {
+        List<List<Integer>> data = new ArrayList<List<Integer>>();
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split("\\s+");
+
+            List<Integer> row = new ArrayList<Integer>(parts.length);
+            for (String part : parts) {
+                row.add(Integer.parseInt(part));
+            }
+
+            data.add(row);
+        }
+
+        return data;
+    }
+}
