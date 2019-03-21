f442cfff501f5ab5eb67357d2a2d5678b71a5322
Andrei Hodanenok <xoda91@gmail.com>
2019-03-22 00:21:58 +0300

    2017: day 6, part 2

--- 2017/06/Day6.java
+++ 2017/06/Day6.java
@@ -12,6 +12,7 @@ public class Day6 {
         int[] blocks = new int[] { 10, 3, 15, 10, 5, 15, 5, 15, 9, 2, 5, 8, 5, 2, 3, 6 };
         //int[] blocks = new int[] { 0, 2, 7, 0 };
         part1(blocks);
+        part2(blocks);
     }
 
     private static void part1(int[] blocks) {
@@ -25,6 +26,28 @@ public class Day6 {
             move(state);
             steps++;
         } while (!seen.contains(Arrays.toString(state)));
+
+        System.out.println(steps);
+    }
+
+    private static void part2(int[] blocks) {
+        int[] state = new int[blocks.length];
+        System.arraycopy(blocks, 0, state, 0, blocks.length);
+
+        Set<String> seen = new HashSet<String>();
+        do {
+            seen.add(Arrays.toString(state));
+            move(state);
+        } while (!seen.contains(Arrays.toString(state)));
+
+        int[] toFind = new int[state.length];
+        System.arraycopy(state, 0, toFind, 0, toFind.length);
+
+        int steps = 0;
+        do {
+            move(state);
+            steps++;
+        } while (!Arrays.equals(toFind, state));
 
         System.out.println(steps);
     }
