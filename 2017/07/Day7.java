8f4f9eae81403a2f207b1b26883c33c2a36bca7c
Andrei Hodanenok <xoda91@gmail.com>
2019-03-24 11:44:14 +0300

    2017: day 7, part 2

--- 2017/07/Day7.java
+++ 2017/07/Day7.java
@@ -1,17 +1,28 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 
+/**
+ * Advent of Code - Day 7
+ * https://adventofcode.com/2017/day/7
+ */
 public class Day7 {
 
     public static void main(String[] args) {
         List<Program> programs = getPrograms();
-        part1(programs);
+        System.out.println(getRoot(programs).name);
+        System.out.println(balanceWeight(getRoot(programs), programs));
     }
 
     private static void part1(List<Program> programs) {
-        Program root = null;
+        System.out.println(getRoot(programs).name);
+    }
+
+    private static void part2(List<Program> programs) {
+        System.out.println(balanceWeight(getRoot(programs), programs));
+    }
+
+    private static Program getRoot(List<Program> programs) {
         for (int i = 0; i < programs.size(); i++) {
             boolean hasRef = false;
             for (int j = 0; j < programs.size(); j++) {
@@ -22,12 +33,73 @@ public class Day7 {
             }
 
             if (!hasRef) {
-                root = programs.get(i);
-                break;
+                return programs.get(i);
             }
         }
 
-        System.out.println(root.name);
+        return null;
+    }
+
+    private static int balanceWeight(Program root, List<Program> programs) {
+        int[] weights = new int[root.holding.size()];
+        for (int i = 0; i < root.holding.size(); i++) {
+            weights[i] = weight(root.holding.get(i), programs);
+        }
+
+        for (int i = 0; i < weights.length; i++) {
+            int count = 0;
+            for (int j = 0; j < weights.length; j++) {
+                if (weights[i] == weights[j]) {
+                    count++;
+                }
+            }
+
+            // only one weight different from others, others are equal
+            if (count == 1) {
+                Program p = getProgram(root.holding.get(i), programs);
+                // going down while there is no balance
+                int result = balanceWeight(p, programs);
+                if (result == 0) {
+                    int unbalanced = weights[i];
+                    int balanced = weights[(i + 1) % weights.length];
+                    if (unbalanced > balanced) {
+                        return p.weight - (unbalanced - balanced);
+                    } else if (unbalanced < balanced) {
+                        return p.weight + (balanced - unbalanced);
+                    } else {
+                        throw new IllegalStateException("weights are equal");
+                    }
+                } else {
+                    return result;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private static int weight(String programName, List<Program> programs) {
+        Program p = getProgram(programName, programs);
+        if (p.holding.size() == 0) {
+            return p.weight;
+        } else {
+            int w = p.weight;
+            for (String name : p.holding) {
+                w += weight(name, programs);
+            }
+
+            return w;
+        }
+    }
+
+    private static Program getProgram(String programName, List<Program> programs) {
+        for (Program p : programs) {
+            if (p.name.equals(programName)) {
+                return p;
+            }
+        }
+
+        return null;
     }
 
     private static List<Program> getPrograms() {
