bc9dfbd89a57e168aeb4aff2cfc4abb8d5c14656
Andrei Hodanenok <xoda91@gmail.com>
2019-04-03 22:14:22 +0300

    2017: day 13, part 1

---
+++ 2017/13/Day13.java
@@ -0,0 +1,122 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Advent of Code - Day 13
+ * https://adventofcode.com/2017/day/13
+ */
+public class Day13 {
+
+    public static void main(String[] args) {
+        List<Layer> layers = getLayers();
+        part1(layers);
+    }
+
+    private static List<Layer> getLayers() {
+        List<Layer> layers = new ArrayList<Layer>();
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String[] parts = scanner.nextLine().split(":");
+            Layer def = new Layer();
+            def.depth = Integer.parseInt(parts[0].trim());
+            def.range = Integer.parseInt(parts[1].trim());
+            layers.add(def);
+        }
+
+        return layers;
+    }
+
+    private static void part1(List<Layer> layers) {
+        Firewall fw = new Firewall(layers);
+
+        int severity = 0;
+        for (Layer layer : sendPacket(fw)) {
+            severity += layer.depth * layer.range;
+        }
+
+        System.out.println(severity);
+    }
+
+    private static List<Layer> sendPacket(Firewall fw) {
+        List<Layer> caught = new ArrayList<Layer>();
+        for (int layerIdx = 0; layerIdx < fw.length(); layerIdx++) {
+            if (fw.hasScannerAt(layerIdx) && fw.scannerPos(layerIdx) == 0) {
+                caught.add(fw.layer(layerIdx));
+            }
+
+            fw.scan();
+        }
+
+        return caught;
+    }
+
+    private static class Firewall {
+
+        private int length;
+        private Map<Integer, LayerState> layers;
+
+        Firewall(List<Layer> layers) {
+            this.layers = new HashMap<Integer, LayerState>();
+            for (Layer layer : layers) {
+                LayerState state = new LayerState();
+                state.layer = layer;
+                this.layers.put(layer.depth, state);
+            }
+
+            int maxDepth = 0;
+            for (Layer layer : layers) {
+                maxDepth = Math.max(maxDepth, layer.depth);
+            }
+            this.length = maxDepth + 1;
+        }
+
+        int length() {
+            return length;
+        }
+
+        boolean hasScannerAt(int layerIdx) {
+            return layers.containsKey(layerIdx);
+        }
+
+        int scannerPos(int layerIdx) {
+            return layers.get(layerIdx).scannerPos;
+        }
+
+        Layer layer(int layerIdx) {
+            return layers.get(layerIdx).layer;
+        }
+
+        void scan() {
+            for (LayerState state : layers.values()) {
+                if (!state.moveTop && state.scannerPos + 1 >= state.layer.range) {
+                    state.moveTop = true;
+                } else if (state.moveTop && state.scannerPos == 0) {
+                    state.moveTop = false;
+                }
+
+                if (state.moveTop) {
+                    state.scannerPos--;
+                } else {
+                    state.scannerPos++;
+                }
+            }
+        }
+    }
+
+    private static class LayerState {
+
+        private boolean moveTop;
+        private int scannerPos;
+        private Layer layer;
+    }
+
+    private static class Layer {
+
+        private int depth;
+        private int range;
+    }
+}
