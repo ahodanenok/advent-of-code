545adcf9a546b3e0adcd40ae8f6eaccf112a3bb8
Andrei Hodanenok <xoda91@gmail.com>
2019-04-08 23:24:00 +0300

    2017: day 17, part 1

---
+++ 2017/17/Day17.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Advent of Code - Day 17
+ * https://adventofcode.com/2017/day/17
+ */
+public class Day17 {
+
+    public static void main(String[] args) {
+        part1(386);
+    }
+
+    private static void part1(int step) {
+        int currPos = 0;
+        List<Integer> buf = new ArrayList<Integer>();
+        buf.add(0);
+
+        for (int n = 1; n <= 2017; n++) {
+            currPos = (currPos + step) % buf.size() + 1;
+            buf.add(currPos, n);
+        }
+
+        System.out.println(buf.get((currPos + 1) % buf.size()));
+    }
+}
