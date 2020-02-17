1362a6e2a59e359dbe49100029300ec80293138d
Andrei Hodanenok <xoda91@gmail.com>
2020-02-18 00:22:51 +0300

    2019: day 6, part 2

--- 2019/06/Day6.java
+++ 2019/06/Day6.java
@@ -1,7 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
 
 /**
  * Advent of Code - Day 6
@@ -12,6 +17,7 @@ public class Day6 {
     public static void main(String[] args) throws Exception {
         Map<String, String> orbits = getOrbits();
         part1(orbits);
+        part2(orbits);
     }
 
     private static void part1(Map<String, String> orbits) {
@@ -24,6 +30,69 @@ public class Day6 {
         }
 
         System.out.println("Part 1: " + count);
+    }
+
+    private static void part2(Map<String, String> orbits) {
+        Map<String, List<String>> orbiters = new HashMap<>();
+        for (Map.Entry<String, String> obj : orbits.entrySet()) {
+            if (orbiters.containsKey(obj.getValue())) {
+                orbiters.get(obj.getValue()).add(obj.getKey());
+            } else {
+                List<String> orb = new ArrayList<>();
+                orb.add(obj.getKey());
+                orbiters.put(obj.getValue(), orb);
+            }
+        }
+
+        String from = orbits.get("YOU");
+        String to = orbits.get("SAN");
+
+        int count = 0;
+
+        Set<String> seen = new HashSet<>();
+        seen.add(from);
+
+        LinkedList<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(from, 0));
+        while (!queue.isEmpty()) {
+            Pair current = queue.removeFirst();
+            if (current.obj.equals(to)) {
+                count = current.steps;
+                break;
+            }
+
+            if (orbits.containsKey(current.obj) && seen.contains(current.obj)) {
+                String other = orbits.get(current.obj);
+                if (!seen.contains(other)) {
+                    queue.addLast(new Pair(other, current.steps + 1));
+                    seen.add(other);
+                }
+            }
+
+            if (orbiters.containsKey(current.obj)) {
+                for (String orb : orbiters.get(current.obj)) {
+                    if (!seen.contains(orb)) {
+                        queue.addLast(new Pair(orb, current.steps + 1));
+                        seen.add(orb);
+                    }
+                }
+            }
+
+            count++;
+        }
+
+        System.out.println("Part 2: " + count);
+    }
+
+    private static class Pair {
+
+        final String obj;
+        final int steps;
+
+        Pair(String obj, int steps) {
+            this.obj = obj;
+            this.steps = steps;
+        }
     }
 
     private static Map<String, String> getOrbits() throws Exception {
