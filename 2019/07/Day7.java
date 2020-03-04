fdaf3def723e9285bb5be9d9b7c473f3774e40b4
Andrei Hodanenok <xoda91@gmail.com>
2020-03-04 22:36:04 +0300

    2019: day 7, part 2

--- 2019/07/Day7.java
+++ 2019/07/Day7.java
@@ -12,7 +12,11 @@ public class Day7 {
 
     public static void main(String[] args) throws Exception {
         List<Integer> ams = getInput();
+        part1(ams);
+        part2(ams);
+    }
 
+    private static void part1(List<Integer> ams) {
         List<Integer> availablePhases = new ArrayList<>();
         availablePhases.add(0);
         availablePhases.add(1);
@@ -25,10 +29,61 @@ public class Day7 {
 
         int currentSignal = Integer.MIN_VALUE;
         for (List<Integer> phases : allPhases) {
-            currentSignal = Math.max(signal(phases, ams), currentSignal);
+            int in = 0;
+            for (int i = 0; i < phases.size(); i++) {
+                Context ctx = new Context(new Memory(ams), Arrays.asList(phases.get(i), in));
+                in = run(ams, ctx); 
+            }
+ 
+            currentSignal = Math.max(in, currentSignal);
         }
 
         System.out.println("Part 1: " + currentSignal);
+    }
+
+    private static void part2(List<Integer> ams) {
+        List<Integer> availablePhases = new ArrayList<>();
+        availablePhases.add(5);
+        availablePhases.add(6);
+        availablePhases.add(7);
+        availablePhases.add(8);
+        availablePhases.add(9);
+
+        List<List<Integer>> allPhases = new ArrayList<>();
+        perms(availablePhases, new ArrayList<>(), allPhases);
+
+        int currentSignal = Integer.MIN_VALUE;
+        for (List<Integer> phases : allPhases) {
+            List<Context> contexts = Arrays.asList(
+                new Context(new Memory(ams), new ArrayList<>()),
+                new Context(new Memory(ams), new ArrayList<>()),
+                new Context(new Memory(ams), new ArrayList<>()),
+                new Context(new Memory(ams), new ArrayList<>()),
+                new Context(new Memory(ams), new ArrayList<>()));
+    
+            for (int i = 0; i < phases.size(); i++) {
+                contexts.get(i).in.add(phases.get(i));
+            }
+            
+
+            int in = 0;
+            int currentAmp = 0;
+            while (true) {
+                contexts.get(currentAmp).in.add(in);
+                Integer out = run(ams, contexts.get(currentAmp));
+                if (out == null) {
+                    break;
+                } 
+
+                in = out;
+                currentAmp = (currentAmp + 1) % 5;
+            }
+
+            currentSignal = Math.max(in, currentSignal);
+        }
+
+        System.out.println("Part 2: " + currentSignal);
+ 
     }
 
     private static void perms(List<Integer> remaining, List<Integer> current, List<List<Integer>> all) {
@@ -43,16 +98,6 @@ public class Day7 {
         }
     }
 
-    private static int signal(List<Integer> phases, List<Integer> ams) {
-        int in = 0;
-        for (int i = 0; i < phases.size(); i++) {
-            Memory m = new Memory(ams);
-            in = run(ams, m, Arrays.asList(phases.get(i), in)).get(0);
-        }
-
-        return in;
-    }
-
     private static List<Integer> getInput() throws Exception {
         List<Integer> input = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
@@ -65,69 +110,67 @@ public class Day7 {
         return input;
     }
 
-    private static List<Integer> run(List<Integer> input, Memory memory, List<Integer> args) {
-        int pos = 0;
-        int inputIdx = 0;
-        List<Integer> out = new ArrayList<>();
-        while (pos < input.size() && memory.get(pos) != 99) {
-            int cmd = memory.get(pos);
+    private static Integer run(List<Integer> input, Context ctx) { 
+        Memory memory = ctx.memory;
+        while (ctx.pos < input.size() && memory.get(ctx.pos) != 99) {
+            int cmd = ctx.memory.get(ctx.pos);
             int opcode = opcode(cmd);
             if (opcode == 1) {
-                int a = value(memory, cmd, pos, 1);
-                int b = value(memory, cmd, pos, 2);
-                memory.set(memory.get(pos + 3), a + b);
-                pos += 4;
+                int a = value(memory, cmd, ctx.pos, 1);
+                int b = value(memory, cmd, ctx.pos, 2);
+                memory.set(memory.get(ctx.pos + 3), a + b);
+                ctx.pos += 4;
             } else if (opcode == 2) {
-                int a = value(memory, cmd, pos, 1);
-                int b = value(memory, cmd, pos, 2);
-                memory.set(memory.get(pos + 3), a * b);
-                pos += 4;
+                int a = value(memory, cmd, ctx.pos, 1);
+                int b = value(memory, cmd, ctx.pos, 2);
+                memory.set(memory.get(ctx.pos + 3), a * b);
+                ctx.pos += 4;
             } else if (opcode == 3) {
-                int in = args.get(inputIdx++);
-                memory.set(memory.get(pos + 1), in);
-                pos += 2;
+                int in = ctx.in.get(ctx.inputIdx++);
+                memory.set(memory.get(ctx.pos + 1), in);
+                ctx.pos += 2;
             } else if (opcode == 4) {
-                //System.out.println(value(memory, cmd, pos, 1));
-                out.add(value(memory, cmd, pos, 1));
-                pos += 2;
+                int out = value(memory, cmd, ctx.pos, 1);
+                ctx.pos += 2;
+                return out;
             } else if (opcode == 5) {
-                int v = value(memory, cmd, pos, 1);
+                int v = value(memory, cmd, ctx.pos, 1);
                 if (v != 0) {
-                    pos = value(memory, cmd, pos, 2);
+                    ctx.pos = value(memory, cmd, ctx.pos, 2);
                 } else {
-                    pos += 3;
+                    ctx.pos += 3;
                 }
             } else if (opcode == 6) {
-                int v = value(memory, cmd, pos, 1);
+                int v = value(memory, cmd, ctx.pos, 1);
                 if (v == 0) {
-                    pos = value(memory, cmd, pos, 2);
+                    ctx.pos = value(memory, cmd, ctx.pos, 2);
                 } else {
-                    pos += 3;
+                    ctx.pos += 3;
                 }
             } else if (opcode == 7) {
-                int a = value(memory, cmd, pos, 1);
-                int b = value(memory, cmd, pos, 2);
+                int a = value(memory, cmd, ctx.pos, 1);
+                int b = value(memory, cmd, ctx.pos, 2);
                 if (a < b) {
-                    memory.set(memory.get(pos + 3), 1);
+                    memory.set(memory.get(ctx.pos + 3), 1);
                 } else {
-                    memory.set(memory.get(pos + 3), 0);
+                    memory.set(memory.get(ctx.pos + 3), 0);
                 }
-                pos += 4;
+                ctx.pos += 4;
             } else if (opcode == 8) {
-                int a = value(memory, cmd, pos, 1);
-                int b = value(memory, cmd, pos, 2);
+                int a = value(memory, cmd, ctx.pos, 1);
+                int b = value(memory, cmd, ctx.pos, 2);
                 if (a == b) {
-                    memory.set(memory.get(pos + 3), 1);
+                    memory.set(memory.get(ctx.pos + 3), 1);
                 } else {
-                    memory.set(memory.get(pos + 3), 0);
+                    memory.set(memory.get(ctx.pos + 3), 0);
                 }
-                pos += 4;
+                ctx.pos += 4;
             } else {
                 throw new IllegalStateException("Unknown command: " + opcode); 
             } 
         }
 
-        return out;
+        return null;
     }
 
     private static int opcode(int instruction) {
@@ -141,6 +184,19 @@ public class Day7 {
         } else {
             return memory.get(pos + idx);
         } 
+    }
+
+    private static class Context {
+
+        private Memory memory;
+        private int pos = 0;
+        private int inputIdx = 0;
+        private List<Integer> in;
+
+        Context(Memory memory, List<Integer> in) {
+            this.memory = memory;
+            this.in = in;
+        }
     }
 
     private static class Memory {
