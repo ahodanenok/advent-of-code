2dcc688d39d2c95995430a4f299735a023ac5d33
Andrei Hodanenok <xoda91@gmail.com>
2020-03-17 00:27:42 +0300

    2019: day 10, part 2

--- 2019/10/Day10.java
+++ 2019/10/Day10.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
  * Advent of Code - Day 10 
@@ -12,43 +13,82 @@ public class Day10 {
     public static void main(String[] args) throws Exception {
         List<Point> asteroids = getInput();
         part1(asteroids);
+        part2(asteroids);
     }
 
     private static void part1(List<Point> asteroids) {
-        int maxDetectCount = Integer.MIN_VALUE;
+        int maxDetectableCount = Integer.MIN_VALUE;
         Point station = null;
         for (Point from : asteroids) {
-            int detectCount = 0;
-
-            next:
-            for (Point to : asteroids) {
-                if (from == to) continue;
-
-                for (Point a : asteroids) {
-                     if (a == to) continue;
-                     if (a == from) continue;
-
-                     if (sameLine(from, to, a) 
-                         && a.distanceTo(from) < to.distanceTo(from)
-                         && a.x >= Math.min(from.x, to.x)
-                         && a.x <= Math.max(from.x, to.x)
-                         && a.y >= Math.min(from.y, to.y)
-                         && a.y <= Math.max(from.y, to.y)) { 
-                             //System.out.println("same line: " + from + ", " + to + ", " + a);
-                             continue next;
-                     }
-                }
-
-                detectCount++;
-            }
-
-            if (detectCount > maxDetectCount) {
+            List<Point> detectable = detectableAsteroids(asteroids, from);
+            if (detectable.size() > maxDetectableCount) {
                 station = from;
-                maxDetectCount = detectCount;
+                maxDetectableCount = detectable.size();
             }
         }
 
-        System.out.println("Part 1: " + maxDetectCount);
+        System.out.println("Part 1: " + maxDetectableCount + ", station at " + station);
+    }
+
+    private static void part2(List<Point> asteroids) {
+        asteroids = new ArrayList<>(asteroids);
+
+        int vaporizedCount = 0;
+        Point station = new Point(23, 19);
+
+        Comparator<Point> clockwise = new Comparator<Point>() {
+            @Override
+            public int compare(Point a, Point b) {
+                if (a.x < station.x && b.x >= station.x) {
+                    return 1;
+                } else if (a.x >= station.x && b.x < station.x) {
+                    return -1;
+                } else {
+                    return (a.x - station.x) * (station.y - b.y) - (b.x - station.x) * (station.y - a.y);
+                }
+            }
+        };
+
+        List<Point> detectable;
+        while (!(detectable = detectableAsteroids(asteroids, station)).isEmpty()) {
+            detectable.sort(clockwise);
+            for (Point a : detectable) {
+                asteroids.remove(a);
+                vaporizedCount++;
+                if (vaporizedCount == 200) {
+                    System.out.println("Part 2: " + ((a.x * 100) + a.y));
+                    return;
+                }
+            }
+        }        
+    }
+
+    private static List<Point> detectableAsteroids(List<Point> asteroids, Point from) {
+        List<Point> detectable = new ArrayList<>();
+
+        next:
+        for (Point to : asteroids) {
+            if (from.equals(to)) continue;
+
+            for (Point a : asteroids) {
+                 if (a.equals(to)) continue;
+                 if (a.equals(from)) continue;
+
+                 if (sameLine(from, to, a) 
+                     && a.distanceTo(from) < to.distanceTo(from)
+                     && a.x >= Math.min(from.x, to.x)
+                     && a.x <= Math.max(from.x, to.x)
+                     && a.y >= Math.min(from.y, to.y)
+                     && a.y <= Math.max(from.y, to.y)) { 
+                         //System.out.println("same line: " + from + ", " + to + ", " + a);
+                         continue next;
+                 }
+            }
+
+            detectable.add(to);
+        }
+
+        return detectable;
     }
 
     private static boolean sameLine(Point a, Point b, Point c) {
@@ -92,6 +132,17 @@ public class Day10 {
         @Override
         public String toString() {
              return "(" + x + ", " + y + ")";
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Point other = (Point) obj;
+            return x == other.x && y == other.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return 31 * x + y;
         }
     }
 }
