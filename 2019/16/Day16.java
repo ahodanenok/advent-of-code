449698a79599e5deb3ea2e13279bdff630fb1869
Andrei Hodanenok <xoda91@gmail.com>
2020-03-28 22:33:30 +0300

    2019: day 16, part 2

--- 2019/16/Day16.java
+++ 2019/16/Day16.java
@@ -7,47 +7,82 @@ import java.util.Arrays;
 public class Day16 {
 
     public static void main(String[] args) {
-        int[] result = phases(getSignal(), 100); 
-        System.out.println("Part 1: " + toString(java.util.Arrays.copyOfRange(result, 0, 8)));
+        part1();
+        part2();
     }
 
-    private static String toString(int[] arr) {
+    private static void part1() {
+        byte[] signal = getSignal(1);
+        byte[] output = phases(signal, 0, 100);
+        System.out.println("Part 1: " + toString(Arrays.copyOfRange(output, 0, 8)));
+    }
+
+    private static void part2() {
+        byte[] signal = getSignal(10000);
+        int offset = Integer.parseInt(toString(Arrays.copyOfRange(signal, 0, 7)));
+        byte[] output = phases(signal, offset, 100);
+        System.out.println("Part 2: " + toString(Arrays.copyOfRange(output, offset, offset + 8)));
+    }
+
+    private static String toString(byte[] arr) {
         StringBuilder sb = new StringBuilder();
-        for (int i : arr) {
+        for (byte i : arr) {
             sb.append(i);
         }
 
         return sb.toString();
     }
 
-    private static int[] getSignal() {
+    private static byte[] getSignal(int repeats) {
         String input = "59717238168580010599012527510943149347930742822899638247083005855483867484356055489419913512721095561655265107745972739464268846374728393507509840854109803718802780543298141398644955506149914796775885246602123746866223528356493012136152974218720542297275145465188153752865061822191530129420866198952553101979463026278788735726652297857883278524565751999458902550203666358043355816162788135488915722989560163456057551268306318085020948544474108340969874943659788076333934419729831896081431886621996610143785624166789772013707177940150230042563041915624525900826097730790562543352690091653041839771125119162154625459654861922989186784414455453132011498";
-
-        int[] signal = new int[input.length()];
-        for (int i = 0; i < input.length(); i++) {
-            signal[i] = Integer.parseInt(input.substring(i, i + 1));
+ 
+        byte[] signal = new byte[input.length() * repeats];
+        for (int i = 0; i < signal.length; i++) {
+            signal[i] = Byte.parseByte(input.charAt(i % input.length()) + "");
         }
 
         return signal;
     }
 
-    private static int[] phases(int[] signal, int count) {
-        int[] pattern = new int[] { 0, 1, 0, -1 };
+    private static byte[] phases(byte[] signal, int offset, int count) {
+        byte[] pattern = new byte[] { 0, 1, 0, -1 };
 
-        int[] input = signal;
-        int[] output = null;
+        byte[] input = signal;
+        byte[] output = null;
         for (int phase = 0; phase < count; phase++) {
-            output = new int[input.length];
+            output = new byte[input.length];
 
-            for (int i = 0; i < input.length; i++) {
+            int i = offset;
+            while (i < input.length / 2) {
                 int element = 0;
-                for (int j = 0; j < input.length; j++) {
+
+                for (int j = i; j < input.length; j++) {
                     int p = (j + 1) / (i + 1) % pattern.length;
                     element += input[j] * pattern[p];
-
                 }
 
-                output[i] = Math.abs(element % 10);
+                output[i] = (byte) Math.abs(element % 10);
+                i++;
+            }
+           
+            // After the first half pattern will be zeroes following by ones: 
+            //  1
+            //  11
+            //  111
+            //  1111
+            //  11111
+            //  etc
+            if (i < input.length) {
+                int sum = 0;
+                for (int j = i; j < input.length; j++) {
+                    sum += input[j];
+                }
+
+                while (i < input.length) {
+                    output[i] = (byte) (sum % 10);
+                    sum -= input[i];
+                    i++;
+                }
             }
 
             input = output;
