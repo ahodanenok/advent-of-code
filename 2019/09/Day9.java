5dac4e45ad6b02f3df66313c860fcf43b9819e23
Andrei Hodanenok <xoda91@gmail.com>
2020-04-16 18:00:16 +0300

    2019: day 9, use intcode library

--- 2019/09/Day9.java
+++ 2019/09/Day9.java
@@ -1,7 +1,6 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.List;
-import java.util.ArrayList;
+import ahodanenok.aoc.intcode.IntcodeComputer;
+import ahodanenok.aoc.intcode.Out;
+import ahodanenok.aoc.intcode.WIn;
 
 /**
  * Advent of Code - Day 9
@@ -10,147 +9,32 @@ import java.util.ArrayList;
 public class Day9 {
 
     public static void main(String[] args) throws Exception {
-        List<Integer> input = getInput();
-        part1(input);
-        part2(input);
+        long[] program = IntcodeComputer.load("input.txt");
+        part1(program);
+        part2(program);
     }
 
-    private static void part1(List<Integer> input) {
-        Context ctx = new Context();
-        ctx.memory = new Memory(input);
-        run(ctx, input, 1);
-    }
-
-    private static void part2(List<Integer> input) {
-        Context ctx = new Context();
-        ctx.memory = new Memory(input);
-        run(ctx, input, 2);
-    }
-
-    private static List<Integer> getInput() throws Exception {
-        List<Integer> input = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
-            String line = reader.readLine();
-            for (String n : line.split(",")) {
-                input.add(Integer.parseInt(n.trim()));
+    private static void part1(long[] program) {
+        IntcodeComputer pc = new IntcodeComputer(program);
+        pc.setIn(new WIn(1));
+        pc.setOut(new Out() {
+            @Override
+            public void write(long n) {
+                System.out.println("Part 1: " + n);
             }
-        }
-
-        return input;
+        });
+        pc.run();
     }
 
-    private static void run(Context ctx, List<Integer> input, int id) {
-        while (ctx.pos < input.size() && ctx.memory.get(ctx.pos) != 99) {
-            int cmd = (int) ctx.memory.get(ctx.pos);
-            int opcode = opcode(cmd);
-            if (opcode == 1) {
-                long a = value(ctx, cmd, 1);
-                long b = value(ctx, cmd, 2);
-                ctx.memory.set(addr(ctx, cmd, 3), a + b);
-                ctx.pos += 4;
-            } else if (opcode == 2) {
-                long a = value(ctx, cmd, 1);
-                long b = value(ctx, cmd, 2);
-                ctx.memory.set(addr(ctx, cmd, 3), a * b);
-                ctx.pos += 4;
-            } else if (opcode == 3) {
-                int in = id;
-                ctx.memory.set(addr(ctx, cmd, 1), in);
-                ctx.pos += 2;
-            } else if (opcode == 4) {
-                System.out.println(value(ctx, cmd, 1));
-                ctx.pos += 2;
-            } else if (opcode == 5) {
-                long v = value(ctx, cmd, 1);
-                if (v != 0) {
-                    ctx.pos = (int) value(ctx, cmd, 2);
-                } else {
-                    ctx.pos += 3;
-                }
-            } else if (opcode == 6) {
-                long v = value(ctx, cmd, 1);
-                if (v == 0) {
-                    ctx.pos = (int) value(ctx, cmd, 2);
-                } else {
-                    ctx.pos += 3;
-                }
-            } else if (opcode == 7) {
-                long a = value(ctx, cmd, 1);
-                long b = value(ctx, cmd, 2);
-                if (a < b) {
-                    ctx.memory.set(addr(ctx, cmd, 3), 1);
-                } else {
-                    ctx.memory.set(addr(ctx, cmd, 3), 0);
-                }
-                ctx.pos += 4;
-            } else if (opcode == 8) {
-                long a = value(ctx, cmd, 1);
-                long b = value(ctx, cmd, 2);
-                if (a == b) {
-                    ctx.memory.set(addr(ctx, cmd, 3), 1);
-                } else {
-                    ctx.memory.set(addr(ctx, cmd, 3), 0);
-                }
-                ctx.pos += 4;
-            } else if (opcode == 9) {
-                ctx.relativeBase += (int) value(ctx, cmd, 1);
-                ctx.pos += 2;
-            } else {
-                throw new IllegalStateException("Unknown command: " + opcode); 
-            } 
-        }
-    }
-
-    private static int opcode(int instruction) {
-        return instruction % 100;
-    }
-
-    private static int addr(Context ctx, int instruction, int idx) {
-        int mode = instruction / (int) Math.pow(10, idx + 1) % 10;
-        if (mode == 0) {
-            return (int) ctx.memory.get(ctx.pos + idx);
-        } else if (mode == 1) {
-            return ctx.pos + idx;
-        } else if (mode == 2) {
-            return ctx.relativeBase + (int) ctx.memory.get(ctx.pos + idx);
-        } else {
-            throw new IllegalArgumentException("mode");
-        }
-    }
-
-    private static long value(Context ctx, int instruction, int idx) {
-        return ctx.memory.get(addr(ctx, instruction, idx));
-    }
-
-    private static class Context {
-
-        Memory memory;
-        int relativeBase;
-        int pos;
-    }
-
-    private static class Memory {
-
-        private List<Long> data; 
-
-        Memory(List<Integer> initialData) {
-            this.data = new ArrayList<>();
-            for (int d : initialData) this.data.add((long) d);
-        }
-
-        void set(int idx, long value) {
-            expandIfNeeded(idx);
-            data.set(idx, value);
-        }
-
-        long get(int idx) {
-            expandIfNeeded(idx);
-            return data.get(idx);
-        }
-
-        private void expandIfNeeded(int idx) {
-            while (data.size() <= idx) data.add(0L);
-        }
+    private static void part2(long[] program) {
+        IntcodeComputer pc = new IntcodeComputer(program);
+        pc.setIn(new WIn(2));
+        pc.setOut(new Out() {
+            @Override
+            public void write(long n) {
+                System.out.println("Part 2: " + n);
+            }
+        });
+        pc.run();
     }
 }
-
