1e33b941113bc41767e6dc00e14d96ffb4210ede
Andrei Hodanenok <xoda91@gmail.com>
2020-12-08 23:44:40 +0300

    2020: day 6, part 2

--- 2020/06/Day6.java
+++ 2020/06/Day6.java
@@ -4,18 +4,55 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 
+/**
+ * Advent of Code - Day 6
+ * https://adventofcode.com/2020/day/6
+ */
 public class Day6 {
 
     public static void main(String[] args) throws Exception {
         List<Group> groups = getGroups();
+        part1(groups);
+        part2(groups);
+    }
 
+    private static void part1(List<Group> groups) {
         int sum = 0;
         for (Group g : groups) {
-            sum += g.yesAnswers.size();
+            Set<Character> uniqueAnswers = new HashSet<>();
+            for (String line : g.answers) {
+                for (int i = 0; i < line.length(); i++) {
+                    uniqueAnswers.add(line.charAt(i));
+                }
+            }
+
+            sum += uniqueAnswers.size();
         }
 
         System.out.println("Part 1: " + sum);
+    }
+
+    private static void part2(List<Group> groups) {
+        int sum = 0;
+        for (Group g : groups) {
+            Map<Character, Integer> answersCount = new HashMap<>();
+            for (String line : g.answers) {
+                for (int i = 0; i < line.length(); i++) {
+                    answersCount.merge(line.charAt(i), 1, Integer::sum);
+                }
+            }
+
+            for (Integer count : answersCount.values()) {
+                if (count == g.answers.size()) {
+                    sum++;
+                }
+            }
+        }
+
+        System.out.println("Part 2: " + sum);
     }
 
     private static List<Group> getGroups() throws Exception {
@@ -31,9 +68,7 @@ public class Day6 {
                     continue;
                 }
 
-                for (int i = 0; i < line.length(); i++) {
-                    currentGroup.yesAnswers.add(line.charAt(i));
-                }
+                currentGroup.answers.add(line);
             }
 
             groups.add(currentGroup);
@@ -43,6 +78,6 @@ public class Day6 {
     }
 
     private static class Group {
-        Set<Character> yesAnswers = new HashSet<>();
+        List<String> answers = new ArrayList<>();
     }
 }
