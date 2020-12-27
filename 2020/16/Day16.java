9b3c7927807d95756b2dadc53e997d44cb1f2382
Andrei Hodanenok <xoda91@gmail.com>
2020-12-27 23:35:47 +0300

    2020: day 16, part 2

--- 2020/16/Day16.java
+++ 2020/16/Day16.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Advent of Code - Day 16
@@ -11,22 +13,92 @@ public class Day16 {
 
     public static void main(String[] args) throws Exception {
         Input input = getInput();
+        part1(input);
+        part2(input);
+    }
 
+    private static void part1(Input input) {
         int errorRate = 0;
         for (Ticket ticket : input.nearbyTickets) {
-            nextValue:
-            for (Integer fieldValue : ticket.fieldValues) {
-                for (Rule rule : input.rules) {
-                    if (rule.isValid(fieldValue)) {
-                        continue nextValue;
-                    }
-                }
-
-                errorRate += fieldValue;
+            int ticketErrorRate = ticket.errorRate(input.rules);
+            if (ticketErrorRate != -1) {
+                errorRate += ticketErrorRate;
             }
         }
 
         System.out.println("Part 1: " + errorRate);
+    }
+
+    private static void part2(Input input) {
+        List<Ticket> tickets = new ArrayList<>();
+        tickets.add(input.myTicket);
+
+        // collect only valid tickets
+        for (Ticket ticket : input.nearbyTickets) {
+            if (ticket.errorRate(input.rules) == -1) {
+                tickets.add(ticket);
+            }
+        }
+
+        int fieldCount = input.myTicket.fieldValues.size();
+        Map<Integer, List<Rule>> fields = new HashMap<>();
+        // collect matching rules by field
+        for (int i = 0; i < fieldCount; i++) {
+            List<Rule> rules = new ArrayList<>();
+
+            nextRule:
+            for (Rule rule : input.rules) {
+                for (Ticket ticket : tickets) {
+                    if (!rule.isValid(ticket.field(i))) {
+                        continue nextRule;
+                    }
+                }
+
+                rules.add(rule);
+            }
+
+            if (fields.containsKey(i)) {
+                fields.get(i).retainAll(rules);
+            } else {
+                fields.put(i, rules);
+            }
+        }
+
+        //for (Map.Entry<Integer, List<Rule>> entry : fields.entrySet()) {
+        //    System.out.println(entry.getKey() + ": " + entry.getValue().stream().map(r -> r.fieldName).collect(java.util.stream.Collectors.toList()));
+        //}
+
+        // resolve fields
+        while (true) {
+            boolean anyChanged = false;
+            for (Map.Entry<Integer, List<Rule>> current : fields.entrySet()) {
+                if (current.getValue().size() == 1) {
+                    for (Map.Entry<Integer, List<Rule>> next : fields.entrySet()) {
+                        if (next.getValue().size() > 1 && next.getValue().remove(current.getValue().get(0))) {
+                            anyChanged = true;
+                        }
+                    }
+                }
+            }
+
+            if (!anyChanged) {
+                break;
+            }
+        }
+
+        //System.out.println();
+        //for (Map.Entry<Integer, List<Rule>> entry : fields.entrySet()) {
+        //    System.out.println(entry.getKey() + ": " + entry.getValue().stream().map(r -> r.fieldName).collect(java.util.stream.Collectors.toList()));
+        //}
+
+        long mul = 1;
+        for (Map.Entry<Integer, List<Rule>> entry : fields.entrySet()) {
+            if (entry.getValue().get(0).fieldName.startsWith("departure")) {
+                mul *= input.myTicket.field(entry.getKey());
+            }
+        }
+
+        System.out.println("Part 2: " + mul);
     }
 
     private static Input getInput() throws Exception {
@@ -119,6 +191,12 @@ public class Day16 {
         }
 
         @Override
+        public boolean equals(Object obj) {
+            Rule other = (Rule) obj;
+            return fieldName.equals(other.fieldName);
+        }
+
+        @Override
         public String toString() {
             StringBuilder sb = new StringBuilder();
             sb.append(fieldName);
@@ -137,5 +215,32 @@ public class Day16 {
     private static class Ticket {
 
         final List<Integer> fieldValues = new ArrayList<>();
+
+        int field(int idx) {
+            return fieldValues.get(idx);
+        }
+
+        int errorRate(List<Rule> rules) {
+            boolean valid = true;
+            int errorRate = 0;
+
+            next:
+            for (Integer fieldValue : fieldValues) {
+                for (Rule rule : rules) {
+                    if (rule.isValid(fieldValue)) {
+                        continue next;
+                    }
+                }
+
+                errorRate += fieldValue;
+                valid = false;
+            }
+
+            if (!valid) {
+                return errorRate;
+            } else {
+                return -1;
+            }
+        }
     }
 }
