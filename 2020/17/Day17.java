9c2b4f4cb3e90b316b5c690e3f761e6670cb4ad2
Andrei Hodanenok <xoda91@gmail.com>
2020-12-28 23:01:36 +0300

    2020: day 17, part 2

--- 2020/17/Day17.java
+++ 2020/17/Day17.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.function.Function;
 
 /**
  * Advent of Code - Day 17
@@ -13,12 +14,58 @@ public class Day17 {
 
     public static void main(String[] args) throws Exception {
         Map<Location, Boolean> cubes = getCubes();
+        part1(cubes);
+        part2(cubes);
+    }
 
-        Map<Location, Boolean> currentCubes = new HashMap<>(cubes);
+    private static void part1(Map<Location, Boolean> cubes) {
+        int activeCount = simulate(cubes, location -> {
+            List<Location> list = new ArrayList<>();
+            for (int dz = -1; dz < 2; dz++) {
+                for (int dy = -1; dy < 2; dy++) {
+                    for (int dx = -1; dx < 2; dx++) {
+                        Location n = new Location(location.x + dx, location.y + dy, location.z + dz, location.w);
+                        if (!n.equals(location)) {
+                            list.add(n);
+                        }
+                    }
+                }
+            }
+
+            return list;
+        });
+
+        System.out.println("Part 1: " + activeCount);
+    }
+
+    private static void part2(Map<Location, Boolean> cubes) {
+        int activeCount = simulate(cubes, location -> {
+            List<Location> list = new ArrayList<>();
+            for (int dz = -1; dz < 2; dz++) {
+                for (int dy = -1; dy < 2; dy++) {
+                    for (int dx = -1; dx < 2; dx++) {
+                        for (int dw = -1; dw < 2; dw++) {
+                            Location n = new Location(location.x + dx, location.y + dy, location.z + dz, location.w + dw);
+                            if (!n.equals(location)) {
+                                list.add(n);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return list;
+        });
+
+        System.out.println("Part 2: " + activeCount);
+    }
+
+    private static int simulate(Map<Location, Boolean> cubes, Function<Location, List<Location>> neighbours) {
+        Map<Location, Boolean> currentCubes = cubes;
         for (int i = 0; i < 6; i++) {
             Map<Location, Boolean> considerCubes = new HashMap<>(currentCubes);
             for (Location location : currentCubes.keySet()) {
-                for (Location neighbour : location.neighbours()) {
+                for (Location neighbour : neighbours.apply(location)) {
                     if (!considerCubes.containsKey(neighbour)) {
                         considerCubes.put(neighbour, false);
                     }
@@ -28,7 +75,7 @@ public class Day17 {
             Map<Location, Boolean> nextCubes = new HashMap<>();
             for (Map.Entry<Location, Boolean> cube : considerCubes.entrySet()) {
                 int nc = 0;
-                for (Location neighbour : cube.getKey().neighbours()) {
+                for (Location neighbour : neighbours.apply(cube.getKey())) {
                     if (considerCubes.getOrDefault(neighbour, false)) {
                         nc++;
                     }
@@ -51,7 +98,7 @@ public class Day17 {
             }
         }
 
-        System.out.println("Part 1: " + activeCount);
+        return activeCount;
     }
 
     private static Map<Location, Boolean> getCubes() throws Exception {
@@ -61,7 +108,7 @@ public class Day17 {
             int y = 0;
             while ((line = reader.readLine()) != null) {
                 for (int x = 0; x < line.length(); x++) {
-                    cubes.put(new Location(x, y, 0), line.charAt(x) == '#');
+                    cubes.put(new Location(x, y, 0, 0), line.charAt(x) == '#');
                 }
 
                 y++;
@@ -76,43 +123,29 @@ public class Day17 {
         final int x;
         final int y;
         final int z;
+        final int w;
 
-        Location(int x, int y, int z) {
+        Location(int x, int y, int z, int w) {
             this.x = x;
             this.y = y;
             this.z = z;
-        }
-
-        List<Location> neighbours() {
-            List<Location> list = new ArrayList<>();
-            for (int dz = -1; dz < 2; dz++) {
-                for (int dy = -1; dy < 2; dy++) {
-                    for (int dx = -1; dx < 2; dx++) {
-                        Location n = new Location(x + dx, y + dy, z + dz);
-                        if (!n.equals(this)) {
-                            list.add(n);
-                        }
-                    }
-                }
-            }
-
-            return list;
+            this.w = w;
         }
 
         @Override
         public boolean equals(Object obj) {
             Location other = (Location) obj;
-            return x == other.x && y == other.y && z == other.z;
+            return x == other.x && y == other.y && z == other.z && w == other.w;
         }
 
         @Override
         public int hashCode() {
-            return 31 * 31 * x + 31 * y + z;
+            return 31 * 31 * 31 * x + 31 * 31 * y + 31 * z + w;
         }
 
         @Override
         public String toString() {
-            return String.format("(%d, %d, %d)", x, y, z);
+            return String.format("(%d, %d, %d, %d)", x, y, z, w);
         }
     }
 }
