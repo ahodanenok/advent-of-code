02a1240a58e3c0988a20995b5c95c346f14604e8
Andrei Hodanenok <xoda91@gmail.com>
2021-01-02 00:23:32 +0300

    2020: day 20, part 2

--- 2020/20/Day20.java
+++ 2020/20/Day20.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Advent of Code - Day 20
@@ -14,11 +15,13 @@ import java.util.Arrays;
  */
 public final class Day20 {
 
-    private static final int TILE_SIZE = 10;
-
     public static void main(String[] args) throws Exception {
         List<Tile> tiles = getTiles();
+        part1(tiles);
+        part2(tiles);
+    }
 
+    private static void part1(List<Tile> tiles) {
         int gridSize = (int) Math.sqrt(tiles.size());
         Map<Point, Tile> placed = new HashMap<>();
         assemble(new Point(0, 0), placed, gridSize, tiles);
@@ -30,6 +33,71 @@ public final class Day20 {
         checksum *= placed.get(new Point(gridSize - 1, gridSize - 1)).num;
 
         System.out.println("Part 1: " + checksum);
+    }
+
+    private static void part2(List<Tile> tiles) {
+        int gridSize = (int) Math.sqrt(tiles.size());
+        Map<Point, Tile> placed = new HashMap<>();
+        assemble(new Point(0, 0), placed, gridSize, tiles);
+
+        int tileSizeN = tiles.get(0).size - 2;
+        Tile image = new Tile(0, gridSize * tileSizeN);
+        for (Map.Entry<Point, Tile> entry : placed.entrySet()) {
+            Tile tile = entry.getValue();
+            Point tilePoint = entry.getKey();
+            for (int row = 1; row < tile.size - 1; row++) {
+                for (int col = 1; col < tile.size - 1; col++) {
+                    if (tile.isFilled(new Point(row, col))) {
+                        image.fill(new Point(
+                            tilePoint.row * tileSizeN + row - 1,
+                            tilePoint.col * tileSizeN + col - 1));
+                    }
+                }
+            }
+        }
+
+        for (Tile variant : getVariants(image)) {
+            Set<Point> monsterPoints = new HashSet<>();
+            for (int row = 0; row < variant.size; row++) {
+                for (int col = 0; col < variant.size; col++) {
+                    monsterPoints.addAll(checkForMonster(variant, new Point(row, col)));
+                }
+            }
+
+            if (!monsterPoints.isEmpty()) {
+                System.out.println("Part 2: " + (variant.filledCount() - monsterPoints.size()));
+                break;
+            }
+        }
+    }
+
+    private static Set<Point> checkForMonster(Tile tile, Point point) {
+        Set<Point> points = new HashSet<>();
+
+        for (Point current : Arrays.asList(
+                    point,
+                    new Point(point.row + 1, point.col + 1),
+                    new Point(point.row + 1, point.col + 4),
+                    new Point(point.row, point.col + 5),
+                    new Point(point.row, point.col + 6),
+                    new Point(point.row + 1, point.col + 7),
+                    new Point(point.row + 1, point.col + 10),
+                    new Point(point.row, point.col + 11),
+                    new Point(point.row, point.col + 12),
+                    new Point(point.row + 1, point.col + 13),
+                    new Point(point.row + 1, point.col + 16),
+                    new Point(point.row, point.col + 17),
+                    new Point(point.row, point.col + 18),
+                    new Point(point.row, point.col + 19),
+                    new Point(point.row - 1, point.col + 18))) {
+            if (tile.isFilled(current)) {
+                points.add(current);
+            } else {
+                return Collections.emptySet();
+            }
+        }
+
+        return points;
     }
 
     private static boolean assemble(Point point, Map<Point, Tile> placed, int gridSize, List<Tile> tiles) {
@@ -56,9 +124,9 @@ public final class Day20 {
             for (Tile variant : getVariants(tile)) {
                 if (checkTop) {
                     Tile top = placed.get(point.top());
-                    for (int col = 0; col < TILE_SIZE; col++) {
-                        if (variant.filled.contains(new Point(0, col)) 
-                                != top.filled.contains(new Point(TILE_SIZE - 1, col))) {
+                    for (int col = 0; col < variant.size; col++) {
+                        if (variant.isFilled(new Point(0, col)) 
+                                != top.isFilled(new Point(variant.size - 1, col))) {
                             continue nextVariant;
                         }
                     }
@@ -66,9 +134,9 @@ public final class Day20 {
 
                 if (checkLeft) {
                     Tile left = placed.get(point.left());
-                    for (int row = 0; row < TILE_SIZE; row++) {
-                        if (variant.filled.contains(new Point(row, 0)) 
-                                != left.filled.contains(new Point(row, TILE_SIZE - 1))) {
+                    for (int row = 0; row < variant.size; row++) {
+                        if (variant.isFilled(new Point(row, 0)) 
+                                != left.isFilled(new Point(row, variant.size - 1))) {
                             continue nextVariant;
                         }
                     }
@@ -95,6 +163,7 @@ public final class Day20 {
     private static List<Tile> getVariants(Tile tile) {
         // not optimal, but simple
         return Arrays.asList(
+            tile,
             tile.rotateRight(),
             tile.rotateRight().rotateRight(),
             tile.rotateLeft(),
@@ -131,12 +200,12 @@ public final class Day20 {
                 }
 
                 if (tile == null) {
-                    tile = new Tile(Integer.parseInt(line.substring(line.indexOf(' ') + 1, line.length() - 1)));
+                    tile = new Tile(Integer.parseInt(line.substring(line.indexOf(' ') + 1, line.length() - 1)), 10);
                     row = 0;
                     continue;
                 }
 
-                for (int col = 0; col < TILE_SIZE; col++) {
+                for (int col = 0; col < tile.size; col++) {
                     if (line.charAt(col) == '#') {
                         tile.fill(new Point(row, col));
                     }
@@ -153,47 +222,57 @@ public final class Day20 {
     private static final class Tile {
 
         private final int num;
+        private final int size;
         private final Set<Point> filled = new HashSet<>();
 
-        Tile(int num) {
+        Tile(int num, int size) {
             this.num = num;
+            this.size = size;
         }
 
         void fill(Point p) {
             this.filled.add(p);
         }
 
+        boolean isFilled(Point p) {
+            return filled.contains(p);
+        }
+
+        int filledCount() {
+            return filled.size();
+        }
+
         Tile vflip() {
-            Tile tile = new Tile(num);
+            Tile tile = new Tile(num, size);
             for (Point p : filled) {
-                tile.fill(p.vflip(TILE_SIZE));
+                tile.fill(p.vflip(size));
             }
 
             return tile;
         }
 
         Tile hflip() {
-            Tile tile = new Tile(num);
+            Tile tile = new Tile(num, size);
             for (Point p : filled) {
-                tile.fill(p.hflip(TILE_SIZE));
+                tile.fill(p.hflip(size));
             }
 
             return tile;
         }
 
         Tile rotateLeft() {
-            Tile tile = new Tile(num);
+            Tile tile = new Tile(num, size);
             for (Point p : filled) {
-                tile.fill(p.rotateLeft(TILE_SIZE));
+                tile.fill(p.rotateLeft(size));
             }
 
             return tile;
         }
 
         Tile rotateRight() {
-            Tile tile = new Tile(num);
+            Tile tile = new Tile(num, size);
             for (Point p : filled) {
-                tile.fill(p.rotateRight(TILE_SIZE));
+                tile.fill(p.rotateRight(size));
             }
 
             return tile;
@@ -203,9 +282,9 @@ public final class Day20 {
         public String toString() {
             StringBuilder sb = new StringBuilder();
             sb.append("Tile ").append(num).append(":\n");
-            for (int row = 0; row < TILE_SIZE; row++) {
-                for (int col = 0; col < TILE_SIZE; col++) {
-                    sb.append(filled.contains(new Point(row, col)) ? '#' : '.');
+            for (int row = 0; row < size; row++) {
+                for (int col = 0; col < size; col++) {
+                    sb.append(isFilled(new Point(row, col)) ? '#' : '.');
                 }
 
                 sb.append('\n');
