e5e3120dd779cf01a9740b99dfc19fcf3d636bbf
Andrei Hodanenok <xoda91@gmail.com>
2021-01-08 11:29:01 +0300

    2020: day 24, part 2

--- 2020/24/Day24.java
+++ 2020/24/Day24.java
@@ -13,7 +13,56 @@ public class Day24 {
 
     public static void main(String[] args) throws Exception {
         List<String> instructions = getInstructions();
+        part1(instructions);
+        part2(instructions);
+    }
 
+    private static void part1(List<String> instructions) {
+        Set<Point> black = getBlack(instructions);
+        System.out.println("Part 1: " + black.size());
+    }
+
+    private static void part2(List<String> instructions) {
+        Set<Point> black = getBlack(instructions);
+        for (int i = 0; i < 100; i++) {
+            Set<Point> tiles = new HashSet<>(black);
+            for (Point b : black) {
+                tiles.add(b.east());
+                tiles.add(b.west());
+                tiles.add(b.southEast());
+                tiles.add(b.southWest());
+                tiles.add(b.northEast());
+                tiles.add(b.northWest());
+            }
+
+            Set<Point> blackNext = new HashSet<>();
+            for (Point t : tiles) {
+                int nc = 0;
+                if (black.contains(t.east())) nc++;
+                if (black.contains(t.west())) nc++;
+                if (black.contains(t.southEast())) nc++;
+                if (black.contains(t.southWest())) nc++;
+                if (black.contains(t.northEast())) nc++;
+                if (black.contains(t.northWest())) nc++;
+
+                if (black.contains(t)) {
+                    if (nc == 1 || nc == 2) {
+                        blackNext.add(t);
+                    }
+                } else {
+                    if (nc == 2) {
+                        blackNext.add(t);
+                    }
+                }
+            }
+
+            black = blackNext;
+        }
+
+        System.out.println("Part 2: " + black.size());
+    }
+
+    private static Set<Point> getBlack(List<String> instructions) {
         Point reference = new Point(0, 0);
         Set<Point> black = new HashSet<>();
         for (String instruction : instructions) {
@@ -25,7 +74,7 @@ public class Day24 {
             }
         }
 
-        System.out.println("Part 1: " + black.size());
+        return black;
     }
 
     private static Point identify(Point reference, String instruction) {
