fd18e9e49bdb3d352d32a9cff4b722ca67990517
Andrei Hodanenok <xoda91@gmail.com>
2021-12-02 23:05:25 +0300

    2021: day 1, part 2

--- 2021/01/Day1.java
+++ 2021/01/Day1.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Advent of Code - Day 1
@@ -8,20 +10,44 @@ import java.io.FileReader;
 public class Day1 {
 
     public static void main(String[] args) throws Exception {
-        int increasedCount = 0;
-        int previousDepth = Integer.MAX_VALUE;
+        List<Integer> depths = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                int depth = Integer.parseInt(line);
-                if (depth > previousDepth) {
-                    increasedCount++;
-                }
-
-                previousDepth = depth;
+                depths.add(Integer.parseInt(line));
             }
         }
 
+        part1(depths);
+        part2(depths);
+    }
+
+    private static void part1(List<Integer> depths) {
+        int increasedCount = 0;
+        int previousDepth = Integer.MAX_VALUE;
+        for (int depth : depths) {
+            if (depth > previousDepth) {
+                increasedCount++;
+            }
+
+            previousDepth = depth;
+        }
+
         System.out.println("Part 1: " + increasedCount);
+    }
+
+    private static void part2(List<Integer> depths) {
+        int increasedCount = 0;
+        int previousSum = Integer.MAX_VALUE;
+        for (int i = 2; i < depths.size(); i++) {
+            int sum = depths.get(i - 2) + depths.get(i - 1) + depths.get(i);
+            if (sum > previousSum) {
+                increasedCount++;
+            }
+
+            previousSum = sum;
+        }
+
+        System.out.println("Part 2: " + increasedCount);
     }
 }
