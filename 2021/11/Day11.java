f665812525bc9f50ba5ec6f3e8f9165b976183e3
Andrei Hodanenok <xoda91@gmail.com>
2021-12-12 20:34:48 +0300

    2021: day 11, part 2

--- 2021/11/Day11.java
+++ 2021/11/Day11.java
@@ -13,8 +13,8 @@ import java.util.Arrays;
 public class Day11 {
 
     public static void main(String[] args) throws Exception {
-        Map<Point, Integer> octopuses = getInput();
-        part1(octopuses);
+        part1(getInput());
+        part2(getInput());
     }
 
     private static Map<Point, Integer> getInput() throws Exception {
@@ -38,44 +38,58 @@ public class Day11 {
     private static void part1(Map<Point, Integer> octopuses) {
         int flashesCount = 0;
         for (int i = 0; i < 100; i++) {
-            for (Point p : octopuses.keySet()) {
-                octopuses.merge(p, 1, Integer::sum);
-            }
-
-            Set<Point> alreadyFlashed = new HashSet<>();
-            while (true) {
-                Set<Point> willFlash = new HashSet<>();
-                for (Point p : octopuses.keySet()) {
-                    if (octopuses.get(p) > 9 && !alreadyFlashed.contains(p)) {
-                        willFlash.add(p);
-                    }
-                }
-
-                if (willFlash.isEmpty()) {
-                    break;
-                }
-
-                for (Point p : willFlash) {
-                    for (Point n : Arrays.asList(
-                            p.topLeft(), p.top(), p.topRight(), 
-                            p.left(), p.right(),
-                            p.bottomLeft(), p.bottom(), p.bottomRight())) {
-                        if (octopuses.containsKey(n)) {
-                            octopuses.merge(n, 1, Integer::sum);
-                        }
-                    }
-                }
-
-                alreadyFlashed.addAll(willFlash);
-                flashesCount += willFlash.size();
-            }
-
-            for (Point p : alreadyFlashed) {
-                octopuses.put(p, 0);
-            }
+            flashesCount += flash(octopuses).size();
         }
 
         System.out.println("Part 1: " + flashesCount);
+    }
+
+    private static void part2(Map<Point, Integer> octopuses) {
+        int step = 1;
+        while (flash(octopuses).size() != octopuses.size()) {
+            step++;
+        }
+
+        System.out.println("Part 2: " + step);
+    }
+
+    private static Set<Point> flash(Map<Point, Integer> octopuses) {
+        for (Point p : octopuses.keySet()) {
+            octopuses.merge(p, 1, Integer::sum);
+        }
+
+        Set<Point> alreadyFlashed = new HashSet<>();
+        while (true) {
+            Set<Point> willFlash = new HashSet<>();
+            for (Point p : octopuses.keySet()) {
+                if (octopuses.get(p) > 9 && !alreadyFlashed.contains(p)) {
+                    willFlash.add(p);
+                }
+            }
+
+            if (willFlash.isEmpty()) {
+                break;
+            }
+
+            for (Point p : willFlash) {
+                for (Point n : Arrays.asList(
+                        p.topLeft(), p.top(), p.topRight(), 
+                        p.left(), p.right(),
+                        p.bottomLeft(), p.bottom(), p.bottomRight())) {
+                    if (octopuses.containsKey(n)) {
+                        octopuses.merge(n, 1, Integer::sum);
+                    }
+                }
+            }
+
+            alreadyFlashed.addAll(willFlash);
+        }
+
+        for (Point p : alreadyFlashed) {
+            octopuses.put(p, 0);
+        }
+
+        return alreadyFlashed;
     }
 
     private static class Point {
