9bcbc5cb7aea9c565cb2c31c147316b28dac65a6
Andrei Hodanenok <xoda91@gmail.com>
2022-12-02 21:59:14 +0300

    2022: day 2, part 2

--- 2022/02/Day2.java
+++ 2022/02/Day2.java
@@ -12,6 +12,7 @@ public class Day2 {
     public static void main(String[] args) throws Exception {
         List<Round> strategy = getStrategy();
         part1(strategy);
+        part2(strategy);
     }
 
     private static List<Round> getStrategy() throws Exception {
@@ -37,21 +38,25 @@ public class Day2 {
                 }
 
                 Shape myShape;
+                Outcome expectedOutcome;
                 switch (parts[1]) {
                     case "X":
                         myShape = Shape.ROCK;
+                        expectedOutcome = Outcome.LOSE;
                         break;
                     case "Y":
                         myShape = Shape.PAPER;
+                        expectedOutcome = Outcome.DRAW;
                         break;
                     case "Z":
                         myShape = Shape.SCISSORS;
+                        expectedOutcome = Outcome.WIN;
                         break;
                     default:
-                        throw new IllegalArgumentException("Unknown my shape: " + parts[1]);
+                        throw new IllegalArgumentException("Unknown my shape/outcome: " + parts[1]);
                 }
 
-                strategy.add(new Round(opponentShape, myShape));
+                strategy.add(new Round(opponentShape, myShape, expectedOutcome));
             }
         }
 
@@ -62,24 +67,23 @@ public class Day2 {
         int totalScore = 0;
         for (Round round : strategy) {
             totalScore += round.myShape.score;
-
             if (round.myShape == Shape.ROCK) {
                 if (round.opponentShape == Shape.SCISSORS) {
-                    totalScore += 6;
+                    totalScore += Outcome.WIN.score;
                 } else if (round.opponentShape == Shape.ROCK) {
-                    totalScore += 3;
+                    totalScore += Outcome.DRAW.score;
                 }
             } else if (round.myShape == Shape.SCISSORS) {
                 if (round.opponentShape == Shape.PAPER) {
-                    totalScore += 6;
+                    totalScore += Outcome.WIN.score;
                 } else if (round.opponentShape == Shape.SCISSORS) {
-                    totalScore += 3;
+                    totalScore += Outcome.DRAW.score;
                 }
             } else if (round.myShape == Shape.PAPER) {
                 if (round.opponentShape == Shape.ROCK) {
-                    totalScore += 6;
+                    totalScore += Outcome.WIN.score;
                 } else if (round.opponentShape == Shape.PAPER) {
-                    totalScore += 3;
+                    totalScore += Outcome.DRAW.score;
                 }
             }
         }
@@ -87,14 +91,44 @@ public class Day2 {
         System.out.println("Part 1: " + totalScore);
     }
 
+    private static void part2(List<Round> strategy) {
+        int totalScore = 0;
+        for (Round round : strategy) {
+            totalScore += round.expectedOutcome.score;
+            if (round.expectedOutcome == Outcome.LOSE) {
+                if (round.opponentShape == Shape.ROCK) {
+                    totalScore += Shape.SCISSORS.score;
+                } else if (round.opponentShape == Shape.SCISSORS) {
+                    totalScore += Shape.PAPER.score;
+                } else if (round.opponentShape == Shape.PAPER) {
+                    totalScore += Shape.ROCK.score;
+                }
+            } else if (round.expectedOutcome == Outcome.WIN) {
+                if (round.opponentShape == Shape.ROCK) {
+                    totalScore += Shape.PAPER.score;
+                } else if (round.opponentShape == Shape.SCISSORS) {
+                    totalScore += Shape.ROCK.score;
+                } else if (round.opponentShape == Shape.PAPER) {
+                    totalScore += Shape.SCISSORS.score;
+                }
+            } else if (round.expectedOutcome == Outcome.DRAW) {
+                totalScore += round.opponentShape.score;
+            }
+        }
+
+        System.out.println("Part 2: " + totalScore);
+    }
+
     private static class Round {
 
         final Shape opponentShape;
         final Shape myShape;
+        final Outcome expectedOutcome;
 
-        Round(Shape opponentShape, Shape myShape) {
+        Round(Shape opponentShape, Shape myShape, Outcome expectedOutcome) {
             this.opponentShape = opponentShape;
             this.myShape = myShape;
+            this.expectedOutcome = expectedOutcome;
         }
     }
 
@@ -105,6 +139,17 @@ public class Day2 {
         final int score;
 
         Shape(int score) {
+            this.score = score;
+        }
+    }
+
+    private enum Outcome {
+
+        LOSE(0), DRAW(3), WIN(6);
+
+        final int score;
+
+        Outcome(int score) {
             this.score = score;
         }
     }
