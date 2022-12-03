5d4914e6137c0ed7b060ebd58230426f5054d2f4
Andrei Hodanenok <xoda91@gmail.com>
2022-12-03 21:53:39 +0300

    2022: day 3, part 2

--- 2022/03/Day3.java
+++ 2022/03/Day3.java
@@ -12,6 +12,7 @@ public class Day3 {
     public static void main(String[] args) throws Exception {
         List<String> items = getItems();
         part1(items);
+        part2(items);
     }
 
     private static List<String> getItems() throws Exception {
@@ -34,12 +35,40 @@ public class Day3 {
                 char item = rucksack.charAt(i);
                 if (rucksack.indexOf(item, mid) != -1) {
                     prioritySum += getPriority(item);
-                    break; // one dublicate per rucksack
+                    break; // one common item between compartments
                 }
             }
         }
 
         System.out.println("Part 1: " + prioritySum);
+    }
+
+    private static void part2(List<String> items) {
+        int prioritySum = 0;
+
+        nextGroup:
+        for (int r = 0; r < items.size(); r += 3) {
+            String first = items.get(r);
+            for (int i = 0; i < first.length(); i++) {
+                char item = first.charAt(i);
+
+                String second = items.get(r + 1);
+                if (second.indexOf(item) == -1) {
+                    continue;
+                }
+
+                
+                String third = items.get(r + 2);
+                if (third.indexOf(item) == -1) {
+                    continue;
+                }
+
+                prioritySum += getPriority(item);
+                continue nextGroup; // one common item per group
+            }
+        }
+
+        System.out.println("Part 2: " + prioritySum);
     }
 
     private static int getPriority(char item) {
