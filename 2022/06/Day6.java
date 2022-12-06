0b8752acfcfddebd128f800466a5fd7dc58abbde
Andrei Hodanenok <xoda91@gmail.com>
2022-12-06 21:02:33 +0300

    2022: day 6, part 2

--- 2022/06/Day6.java
+++ 2022/06/Day6.java
@@ -11,6 +11,7 @@ public class Day6 {
     public static void main(String[] args) throws Exception {
         String signal = getSignal();
         part1(signal);
+        part2(signal);
     }
 
     private static String getSignal() throws Exception {
@@ -29,6 +30,20 @@ public class Day6 {
 
             if (buffer.size() == 4) {
                 System.out.println("Part 1: " + (i + 1));
+                break;
+            }
+        }
+    }
+
+    private static void part2(String signal) {
+        for (int i = 13; i < signal.length(); i++) {
+            HashSet<Character> buffer =  new HashSet<>();
+            for (int j = i - 13; j <= i; j++) {
+                buffer.add(signal.charAt(j));
+            }
+
+            if (buffer.size() == 14) {
+                System.out.println("Part 2: " + (i + 1));
                 break;
             }
         }
