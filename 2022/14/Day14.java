41a31a491010675522f4bef69883d8fdee187dd2
Andrei Hodanenok <xoda91@gmail.com>
2022-12-17 16:50:58 +0300

    2022: day 14, part 2

--- 2022/14/Day14.java
+++ 2022/14/Day14.java
@@ -14,6 +14,7 @@ public class Day14 {
     public static void main(String[] args) throws Exception {
         List<List<Coordinate>> shapes = getShapes();
         part1(shapes);
+        part2(shapes);
     }
 
     private static void part1(List<List<Coordinate>> shapes) {
@@ -22,7 +23,6 @@ public class Day14 {
         int xMin = sandSource.x;
         int xMax = sandSource.x;
         int yMax = Integer.MIN_VALUE;
-
         for (List<Coordinate> shape : shapes) {
             for (Coordinate coord : shape) {
                 xMin = Math.min(coord.x, xMin);
@@ -32,7 +32,6 @@ public class Day14 {
         }
 
         boolean [][] cave = new boolean[yMax + 1][xMax - xMin + 1];
-
         for (List<Coordinate> shape : shapes) {
             for (int i = 1; i < shape.size(); i++) {
                 Coordinate prev = shape.get(i - 1);
@@ -92,6 +91,78 @@ public class Day14 {
         System.out.println("Part 1: " + sandCount);
     }
 
+    private static void part2(List<List<Coordinate>> shapes) {
+        Coordinate sandSource = new Coordinate(500, 0);
+
+        int width = sandSource.x * 2;
+        int yMax = Integer.MIN_VALUE;
+        for (List<Coordinate> shape : shapes) {
+            for (Coordinate coord : shape) {
+                yMax = Math.max(coord.y, yMax);
+            }
+        }
+        yMax += 2;
+
+        boolean [][] cave = new boolean[yMax + 1][width];
+        for (List<Coordinate> shape : shapes) {
+            for (int i = 1; i < shape.size(); i++) {
+                Coordinate prev = shape.get(i - 1);
+                Coordinate curr = shape.get(i);
+                if (prev.x != curr.x) {
+                    int to = Math.max(prev.x, curr.x);
+                    for (int x = Math.min(prev.x, curr.x); x < to; x++) {
+                        cave[curr.y][x] = true;
+                    }
+                } else if (prev.y != curr.y) {
+                    int to = Math.max(prev.y, curr.y);
+                    for (int y = Math.min(prev.y, curr.y); y < to; y++) {
+                        cave[y][curr.x] = true;
+                    }
+                } else {
+                    throw new IllegalStateException("Bad shape!");
+                }
+
+                cave[prev.y][prev.x] = true;
+                cave[curr.y][curr.x] = true;
+            }
+        }
+        // add floor
+        for (int x = 0; x < width; x++) {
+            cave[yMax][x] = true;
+        }
+
+        int sandCount = 0;
+
+        flow:
+        while (true) {
+            int x = sandSource.x;
+            int y = sandSource.y;
+            while (true) {
+                if (!cave[y + 1][x]) {
+                    y++;
+                } else if (!cave[y + 1][x - 1]) {
+                    x--;
+                    y++;
+                } else if (!cave[y + 1][x + 1]) {
+                    x++;
+                    y++;
+                } else {
+                    cave[y][x] = true;
+                    sandCount++;
+                    if (x == sandSource.x && y == sandSource.y) {
+                        // sand source is blocked
+                        break flow;
+                    } else {
+                        // comes to rest
+                        continue flow;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Part 2: " + sandCount);
+    }
+
     private static List<List<Coordinate>> getShapes() throws Exception {
         List<List<Coordinate>> shapes = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
@@ -119,17 +190,6 @@ public class Day14 {
         Coordinate(int x, int y) {
             this.x = x;
             this.y = y;
-        }
-
-        @Override
-        public int hashCode() {
-            return 31 * x + y;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            Coordinate other = (Coordinate) obj;
-            return x == other.x && y == other.y;
         }
     }
 }
