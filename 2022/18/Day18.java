435e9c04c87edbff7c28a96338595f690a1827a4
Andrei Hodanenok <xoda91@gmail.com>
2022-12-24 21:22:12 +0300

    2022: day 18, part 2

--- 2022/18/Day18.java
+++ 2022/18/Day18.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Set;
 import java.util.HashSet;
 
@@ -12,45 +12,130 @@ import java.util.HashSet;
 public class Day18 {
 
     public static void main(String[] args) throws Exception {
-        List<Cube> scan = getScan();
+        Set<Cube> scan = getScan();
         part1(scan);
+        part2(scan);
     }
 
-    private static void part1(List<Cube> scan) {
+    private static void part1(Set<Cube> scan) {
         int surfaceArea = 0;
+        for (Cube cube : scan) {
+            for (Cube neighbor : List.of(
+                    cube.left(), cube.right(), cube.top(), cube.bottom(), cube.front(), cube.back())) {
 
-        Set<Cube> cubes = new HashSet<>(scan);
-        for (Cube cube : cubes) {
-            if (!cubes.contains(new Cube(cube.x - 1, cube.y, cube.z))) {
-                surfaceArea++;
-            }
-
-            if (!cubes.contains(new Cube(cube.x + 1, cube.y, cube.z))) {
-                surfaceArea++;
-            }
-
-            if (!cubes.contains(new Cube(cube.x, cube.y - 1, cube.z))) {
-                surfaceArea++;
-            }
-
-            if (!cubes.contains(new Cube(cube.x, cube.y + 1, cube.z))) {
-                surfaceArea++;
-            }
-
-            if (!cubes.contains(new Cube(cube.x, cube.y, cube.z - 1))) {
-                surfaceArea++;
-            }
-
-            if (!cubes.contains(new Cube(cube.x, cube.y, cube.z + 1))) {
-                surfaceArea++;
+                if (!scan.contains(neighbor)) {
+                    surfaceArea++;
+                }
             }
         }
 
         System.out.println("Part 1: " + surfaceArea);
     }
 
-    private static List<Cube> getScan() throws Exception {
-        List<Cube> cubes = new ArrayList<>();
+    private static void part2(Set<Cube> scan) {
+        int xMin = Integer.MAX_VALUE;
+        int xMax = Integer.MIN_VALUE;
+        int yMin = Integer.MAX_VALUE;
+        int yMax = Integer.MIN_VALUE;
+        int zMin = Integer.MAX_VALUE;
+        int zMax = Integer.MIN_VALUE;
+        for (Cube cube : scan) {
+            xMin = Math.min(cube.x, xMin);
+            xMax = Math.max(cube.x, xMax);
+            yMin = Math.min(cube.y, yMin);
+            yMax = Math.max(cube.y, yMax);
+            zMin = Math.min(cube.z, zMin);
+            zMax = Math.max(cube.z, zMax);
+        }
+
+        LinkedList<Cube> queue = new LinkedList<>();
+        for (int x = xMin; x <= xMax; x++) {
+            for (int y = yMin; y <= yMax; y++) {
+                Cube steamFront = new Cube(x, y, zMin);
+                if (!scan.contains(steamFront)) {
+                    queue.add(steamFront);
+                }
+
+                Cube steamBack = new Cube(x, y, zMax);
+                if (!scan.contains(steamBack)) {
+                    queue.add(steamBack);
+                }
+            }
+        }
+        for (int x = xMin; x <= xMax; x++) {
+            for (int z = zMin; z <= zMax; z++) {
+                Cube steamBottom = new Cube(x, yMin, z);
+                if (!scan.contains(steamBottom)) {
+                    queue.add(steamBottom);
+                }
+
+                Cube steamTop = new Cube(x, yMax, z);
+                if (!scan.contains(steamTop)) {
+                    queue.add(steamTop);
+                }
+            }
+        }
+        for (int y = yMin; y <= yMax; y++) {
+            for (int z = zMin; z <= zMax; z++) {
+                Cube steamLeft = new Cube(xMin, y, z);
+                if (!scan.contains(steamLeft)) {
+                    queue.add(steamLeft);
+                }
+
+                Cube steamRight = new Cube(xMax, y, z);
+                if (!scan.contains(steamRight)) {
+                    queue.add(steamRight);
+                }
+            }
+        }
+
+        Set<Cube> steam = new HashSet<>();
+        while (!queue.isEmpty()) {
+            Cube cube = queue.removeFirst();
+            if (steam.contains(cube)) {
+                continue;
+            }
+
+            steam.add(cube);
+            for (Cube neighbor : List.of(
+                    cube.left(), cube.right(), cube.top(), cube.bottom(), cube.front(), cube.back())) {
+
+                if (neighbor.x < xMin - 1 || neighbor.x > xMax + 1) {
+                    continue;
+                }
+
+                if (neighbor.y < yMin - 1 || neighbor.y > yMax + 1) {
+                    continue;
+                }
+
+                if (neighbor.z < zMin - 1 || neighbor.z > zMax + 1) {
+                    continue;
+                }
+
+                if (scan.contains(neighbor) || steam.contains(neighbor)) {
+                    continue;
+                }
+
+                queue.add(neighbor);
+            }
+        }
+
+        int surfaceArea = 0;
+        for (Cube cube : scan) {
+            for (Cube neighbor : List.of(
+                    cube.left(), cube.right(), cube.top(), cube.bottom(), cube.front(), cube.back())) {
+
+                if (steam.contains(neighbor)) {
+                    surfaceArea++;
+                }
+            }
+        }
+
+        System.out.println("Part 2: " + surfaceArea);
+    }
+
+    private static Set<Cube> getScan() throws Exception {
+        Set<Cube> cubes = new HashSet<>();
         try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -75,6 +160,30 @@ public class Day18 {
             this.x = x;
             this.y = y;
             this.z = z;
+        }
+
+        Cube left() {
+            return new Cube(x - 1, y, z);
+        }
+
+        Cube right() {
+            return new Cube(x + 1, y, z);
+        }
+
+        Cube top() {
+            return new Cube(x, y + 1, z);
+        }
+
+        Cube bottom() {
+            return new Cube(x, y - 1, z);
+        }
+
+        Cube front() {
+            return new Cube(x, y, z - 1);
+        }
+
+        Cube back() {
+            return new Cube(x, y, z + 1);
         }
 
         @Override
