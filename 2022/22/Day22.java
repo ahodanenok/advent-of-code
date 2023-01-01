4b17e3d0ca1fccba275a10bb30485bf342b7b363
Andrei Hodanenok <xoda91@gmail.com>
2023-01-01 19:30:21 +0300

    2022: day 22, part 2

--- 2022/22/Day22.java
+++ 2022/22/Day22.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -14,16 +15,190 @@ public class Day22 {
     public static void main(String[] args) throws Exception {
         Notes notes = getNotes();
         part1(notes);
+        part2(notes);
     }
 
     private static void part1(Notes notes) {
         Map map = notes.map;
+        System.out.println("Part 1: " + move(notes, new MovingStrategy() {
+
+            @Override
+            public Location next(Location location) {
+                int x = location.x;
+                int y = location.y;
+                do {
+                    x = (map.width + x + location.dir.dx) % map.width;
+                    y = (map.height + y + location.dir.dy) % map.height;
+                } while (map.isVoid(x, y));
+
+                return new Location(x, y, location.dir);
+            }
+        }));
+    }
+
+    private static void part2(Notes notes) {
+        Map map = notes.map;
+
+        HashMap<Side, Side> sides = new HashMap<>();
+
+        //int sideLength = 4;
+        //sides.put(new Side(1, 1, Direction.UP), new Side(2, 0, Direction.LEFT));
+        //sides.put(new Side(2, 1, Direction.RIGHT), new Side(3, 2, Direction.UP));
+        //sides.put(new Side(2, 2, Direction.DOWN), new Side(0, 1, Direction.DOWN));
+
+        int sideLength = 50;
+        sides.put(new Side(1, 0, Direction.UP), new Side(0, 3, Direction.LEFT));
+        sides.put(new Side(1, 0, Direction.LEFT), new Side(0, 2, Direction.LEFT));
+        sides.put(new Side(2, 0, Direction.UP), new Side(0, 3, Direction.DOWN));
+        sides.put(new Side(2, 0, Direction.RIGHT), new Side(1, 2, Direction.RIGHT));
+        sides.put(new Side(2, 0, Direction.DOWN), new Side(1, 1, Direction.RIGHT));
+        sides.put(new Side(1, 1, Direction.LEFT), new Side(0, 2, Direction.UP));
+        sides.put(new Side(1, 1, Direction.RIGHT), new Side(2, 0, Direction.DOWN));
+        sides.put(new Side(0, 2, Direction.UP), new Side(1, 1, Direction.LEFT));
+        sides.put(new Side(0, 2, Direction.LEFT), new Side(1, 0, Direction.LEFT));
+        sides.put(new Side(1, 2, Direction.RIGHT), new Side(2, 0, Direction.RIGHT));
+        sides.put(new Side(1, 2, Direction.DOWN), new Side(0, 3, Direction.RIGHT));
+        sides.put(new Side(0, 3, Direction.LEFT), new Side(1, 0, Direction.UP));
+        sides.put(new Side(0, 3, Direction.RIGHT), new Side(1, 2, Direction.DOWN));
+        sides.put(new Side(0, 3, Direction.DOWN), new Side(2, 0, Direction.UP));
+
+        System.out.println("Part 2: " + move(notes, new MovingStrategy() {
+
+            @Override
+            public Location next(Location location) {
+                int x = location.x + location.dir.dx;
+                int y = location.y + location.dir.dy;
+                if (x >= 0 && x < map.width && y >= 0 && y < map.height && !map.isVoid(x, y)) {
+                    return new Location(x, y, location.dir);
+                }
+
+                Location next = translate(location);
+                if (map.isVoid(next.x, next.y)) {
+                    throw new IllegalStateException(String.format("%s (%d, %d) -> (%d, %d)",
+                        location.dir, location.x, location.y, next.x, next.y));
+                }
+
+                return next;
+            }
+
+            private Location translate(Location location) {
+                int sx = location.x / sideLength;
+                int sy = location.y / sideLength;
+                Side toSide = sides.get(new Side(sx, sy, location.dir));
+
+                int offsetX = location.x - sx * sideLength;
+                int offsetY = location.y - sy * sideLength;
+
+                if (location.dir == Direction.UP) {
+                    if (toSide.dir == Direction.UP) {
+                        return new Location(
+                            toSide.x * sideLength + (sideLength - 1 - offsetX),
+                            toSide.y * sideLength,
+                            Direction.DOWN);
+                    } else if (toSide.dir == Direction.DOWN) {
+                        return new Location(
+                            toSide.x * sideLength + offsetX,
+                            toSide.y * sideLength + (sideLength - 1),
+                            Direction.UP);
+                    } else if (toSide.dir == Direction.LEFT) {
+                        return new Location(
+                            toSide.x * sideLength,
+                            toSide.y * sideLength + offsetX,
+                            Direction.RIGHT);
+                    } else if (toSide.dir == Direction.RIGHT) {
+                        return new Location(
+                            toSide.x * sideLength + (sideLength - 1),
+                            toSide.y * sideLength + (sideLength - 1 - offsetX),
+                            Direction.LEFT);
+                    }
+                } else if (location.dir == Direction.DOWN) {
+                    if (toSide.dir == Direction.UP) {
+                        return new Location(
+                            toSide.x * sideLength + offsetX,
+                            toSide.y * sideLength,
+                            Direction.DOWN);
+                    } else if (toSide.dir == Direction.DOWN) {
+                        return new Location(
+                            toSide.x * sideLength + (sideLength - 1 - offsetX),
+                            toSide.y * sideLength + (sideLength - 1),
+                            Direction.UP);
+                    } else if (toSide.dir == Direction.LEFT) {
+                        return new Location(
+                            toSide.x * sideLength,
+                            toSide.y * sideLength + (sideLength - 1 - offsetX),
+                            Direction.RIGHT);
+                    } else if (toSide.dir == Direction.RIGHT) {
+                        return new Location(
+                            toSide.x * sideLength + (sideLength - 1),
+                            toSide.y * sideLength + offsetX,
+                            Direction.LEFT);
+                    }
+                } else if (location.dir == Direction.LEFT) {
+                    if (toSide.dir == Direction.UP) {
+                        return new Location(
+                            toSide.x * sideLength + offsetY,
+                            toSide.y * sideLength,
+                            Direction.DOWN);
+                    } else if (toSide.dir == Direction.DOWN) {
+                        return new Location(
+                            toSide.x * sideLength + (sideLength - 1 - offsetY),
+                            toSide.y * sideLength + (sideLength - 1),
+                            Direction.UP);
+                    } else if (toSide.dir == Direction.LEFT) {
+                        return new Location(
+                            toSide.x * sideLength,
+                            toSide.y * sideLength + (sideLength - 1 - offsetY),
+                            Direction.RIGHT);
+                    } else if (toSide.dir == Direction.RIGHT) {
+                        return new Location(
+                            toSide.x * sideLength + (sideLength - 1),
+                            toSide.y * sideLength + offsetY,
+                            Direction.LEFT);
+                    }
+                } else if (location.dir == Direction.RIGHT) {
+                    if (toSide.dir == Direction.UP) {
+                        return new Location(
+                            toSide.x * sideLength + (sideLength - 1 - offsetY),
+                            toSide.y * sideLength,
+                            Direction.DOWN);
+                    } else if (toSide.dir == Direction.DOWN) {
+                        return new Location(
+                            toSide.x * sideLength + offsetY,
+                            toSide.y * sideLength + (sideLength - 1),
+                            Direction.UP);
+                    } else if (toSide.dir == Direction.LEFT) {
+                        return new Location(
+                            toSide.x * sideLength,
+                            toSide.y * sideLength + offsetY,
+                            Direction.LEFT);
+                    } else if (toSide.dir == Direction.RIGHT) {
+                        return new Location(
+                            toSide.x * sideLength + (sideLength - 1),
+                            toSide.y * sideLength + (sideLength - 1 - offsetY),
+                            Direction.LEFT);
+                    }
+                }
+
+                throw new IllegalStateException();
+            }
+        }));
+    }
+
+    private static int move(Notes notes, MovingStrategy m) {
+        Map map = notes.map;
         List<Direction> directions =
             List.of(Direction.UP, Direction.RIGHT, Direction.DOWN, Direction.LEFT);
 
-        int y = 0;
-        int x = map.nextRight(0, y);
-        int dirIdx = directions.indexOf(Direction.RIGHT);
+        Location location;
+        {
+            int x = 0;
+            while (map.isVoid(x, 0)) {
+                x++;
+            }
+
+            location = new Location(x, 0, Direction.RIGHT);
+        }
+        int dirIdx = directions.indexOf(location.dir);
 
         /*for (int row = 0; row < map.height; row++) {
             for (int col = 0; col < map.width; col++) {
@@ -38,48 +213,17 @@ public class Day22 {
             System.out.println();
         }*/
 
+        //System.out.printf("%5s (%d, %d)%n", location.dir, location.x, location.y);
         for (Command cmd : notes.path) {
-            Direction dir = directions.get(dirIdx);
-            if (dir == Direction.UP) {
-                int yNext = y;
-                for (int i = 0; i < cmd.steps; i++) {
-                    yNext = map.nextUp(x, yNext);
-                    if (!map.isOpen(x, yNext)) {
-                        break;
-                    }
-
-                    y = yNext;
+            for (int i = 0; i < cmd.steps; i++) {
+                Location next = m.next(location);
+                if (!map.isOpen(next.x, next.y)) {
+                    break;
                 }
-            } else if (dir == Direction.DOWN) {
-                int yNext = y;
-                for (int i = 0; i < cmd.steps; i++) {
-                    yNext = map.nextDown(x, yNext);
-                    if (!map.isOpen(x, yNext)) {
-                        break;
-                    }
-
-                    y = yNext;
-                }
-            } else if (dir == Direction.LEFT) {
-                int xNext = x;
-                for (int i = 0; i < cmd.steps; i++) {
-                    xNext = map.nextLeft(xNext, y);
-                    if (!map.isOpen(xNext, y)) {
-                        break;
-                    }
-
-                    x = xNext;
-                }
-            } else if (dir == Direction.RIGHT) {
-                int xNext = x;
-                for (int i = 0; i < cmd.steps; i++) {
-                    xNext = map.nextRight(xNext, y);
-                    if (!map.isOpen(xNext, y)) {
-                        break;
-                    }
-
-                    x = xNext;
-                }
+ 
+                location = next;
+                dirIdx = directions.indexOf(location.dir);
+                //System.out.printf("%5s (%d, %d)%n", location.dir, location.x, location.y);
             }
 
             if (cmd.turn == Turn.LEFT) {
@@ -87,10 +231,11 @@ public class Day22 {
             } else if (cmd.turn == Turn.RIGHT) {
                 dirIdx = (dirIdx + 1) % directions.size();
             }
+
+            location = new Location(location.x, location.y, directions.get(dirIdx));
         }
 
-        int pwd = 1000 * (y + 1) + 4 * (x + 1) + directions.get(dirIdx).score;
-        System.out.println("Part 1: " + pwd);
+        return 1000 * (location.y + 1) + 4 * (location.x + 1) + directions.get(dirIdx).score;
     }
 
     private static Notes getNotes() throws Exception {
@@ -121,6 +266,35 @@ public class Day22 {
             }
 
             return new Notes(new Map(rows), commands);
+        }
+    }
+
+    private interface MovingStrategy {
+
+        Location next(Location location);
+    }
+
+    private static class Side {
+
+        final int x;
+        final int y;
+        final Direction dir;
+
+        Side(int x, int y, Direction dir) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+        }
+
+        @Override
+        public int hashCode() {
+            return 31 * 31 * x + 31 * y + dir.ordinal();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Side other = (Side) obj;
+            return x == other.x && y == other.y && dir == other.dir;
         }
     }
 
@@ -168,44 +342,25 @@ public class Day22 {
             }
         }
 
+        boolean isVoid(int x, int y) {
+            return tiles[y][x] == VOID_TILE;
+        }
+
         boolean isOpen(int x, int y) {
             return tiles[y][x] == OPEN_TILE;
         }
+    }
 
-        int nextUp(int x, int y) {
-            int yNext = y;
-            do {
-                yNext = (height + yNext - 1) % height;
-            } while (tiles[yNext][x] == VOID_TILE);
+    private static class Location {
 
-            return yNext;
-        }
+        final int x;
+        final int y;
+        final Direction dir;
 
-        int nextDown(int x, int y) {
-            int yNext = y;
-            do {
-                yNext = (yNext + 1) % height;
-            } while (tiles[yNext][x] == VOID_TILE);
-
-            return yNext;
-        }
-
-        int nextLeft(int x, int y) {
-            int xNext = x;
-            do {
-                xNext = (width + xNext - 1) % width;
-            } while (tiles[y][xNext] == VOID_TILE);
-
-            return xNext;
-        }
-
-        int nextRight(int x, int y) {
-            int xNext = x;
-            do {
-                xNext = (xNext + 1) % width;
-            } while (tiles[y][xNext] == VOID_TILE);
-
-            return xNext;
+        Location(int x, int y, Direction dir) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
         }
     }
 
@@ -227,12 +382,16 @@ public class Day22 {
 
     private enum Direction {
 
-        UP(3), DOWN(1), LEFT(2), RIGHT(0);
+        UP(3, 0, -1), DOWN(1, 0, 1), LEFT(2, -1, 0), RIGHT(0, 1, 0);
 
         final int score;
+        final int dx;
+        final int dy;
 
-        Direction(int score) {
+        Direction(int score, int dx, int dy) {
             this.score = score;
+            this.dx = dx;
+            this.dy = dy;
         }
     }
 }
