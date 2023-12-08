035a80235dc516beb24ceb84552519148de2f9a5
Andrei Hodanenok <xoda91@gmail.com>
2023-12-08 22:49:55 +0300

    2023: day 7, part 2

--- 2023/07/Day7.java
+++ 2023/07/Day7.java
@@ -11,28 +11,47 @@ import java.util.HashMap;
  */
 public class Day7 {
 
-    private static final List<Character> CARD_STRENGTH = List.of(
-        '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'
-    );
+    private static final int FIVE_KIND_SCORE  = 100;
+    private static final int FOUR_KIND_SCORE  = 90;
+    private static final int FULL_HOUSE_SCORE = 80;
+    private static final int THREE_KIND_SCORE = 70;
+    private static final int TWO_PAIR_SCORE   = 60;
+    private static final int ONE_PAIR_SCORE   = 50;
+    private static final int HIGH_CARD_SCORE  = 1;
 
     public static void main(String[] args) throws Exception {
         List<Bid> bids = getInput();
         part1(bids);
+        part2(bids);
     }
 
     private static void part1(List<Bid> bids) {
+        System.out.println("Part 1: " + calculateWinnings(
+            new ArrayList<>(bids),
+            List.of('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'), 
+            null));
+    }
+
+    private static void part2(List<Bid> bids) {
+        System.out.println("Part 2: " + calculateWinnings(
+            new ArrayList<>(bids),
+            List.of('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'),
+            'J'));
+    }
+
+    private static long calculateWinnings(List<Bid> bids, List<Character> cardStrength, Character joker) {
         bids.sort((a, b) -> {
             int n = Integer.compare(
-                getHandStrength(a.hand),
-                getHandStrength(b.hand));
+                getHandStrength(a.hand, joker),
+                getHandStrength(b.hand, joker));
             if (n != 0) {
                 return n;
             }
 
             for (int i = 0; i < a.hand.length(); i++) {
                 n = Integer.compare(
-                    CARD_STRENGTH.indexOf(a.hand.charAt(i)),
-                    CARD_STRENGTH.indexOf(b.hand.charAt(i)));
+                    cardStrength.indexOf(a.hand.charAt(i)),
+                    cardStrength.indexOf(b.hand.charAt(i)));
                 if (n != 0) {
                     return n;
                 }
@@ -46,48 +65,53 @@ public class Day7 {
             totalWinnings += (i + 1) * bids.get(i).amount;
         }
 
-        System.out.println("Part 1: " + totalWinnings);
+        return totalWinnings;
     }
 
-    private static int getHandStrength(String hand) {
+    private static int getHandStrength(String hand, Character joker) {
+        int jokerCount = 0;
         Map<Character, Integer> cardCount = new HashMap<>();
         for (int i = 0; i < hand.length(); i++) {
-            cardCount.merge(hand.charAt(i), 1, Integer::sum);
-        }
-
-        if (cardCount.size() == 1) {
-            // five of a kind
-            return 100;
-        }
-
-        if (cardCount.size() == 2) {
-            int n = cardCount.values().iterator().next();
-            if (n == 4 || n == 1) {
-                // four of a kind
-                return 90;
+            if (joker != null && hand.charAt(i) == joker) {
+                jokerCount++;
             } else {
-                // full house
-                return 80;
+                cardCount.merge(hand.charAt(i), 1, Integer::sum);
             }
         }
 
-        if (cardCount.size() == 3) {
-            if (cardCount.values().contains(3)) {
-                // three of a kind
-                return 70;
+        List<Integer> fingerprint = new ArrayList<>(cardCount.values());
+        fingerprint.sort(java.util.Comparator.naturalOrder());
+        if (fingerprint.isEmpty()) {
+            fingerprint.add(jokerCount);
+        } else if (jokerCount > 0) {
+            fingerprint.set(fingerprint.size() - 1, fingerprint.get(fingerprint.size() - 1) + jokerCount);
+        }
+
+        if (fingerprint.size() == 1) {
+            return FIVE_KIND_SCORE;
+        }
+
+        if (fingerprint.size() == 2) {
+            if (fingerprint.get(0) == 1) {
+                return FOUR_KIND_SCORE;
             } else {
-                // two pair
-                return 60;
+                return FULL_HOUSE_SCORE;
             }
         }
 
-        if (cardCount.size() == 4) {
-            // one pair
-            return 50;
+        if (fingerprint.size() == 3) {
+            if (fingerprint.get(fingerprint.size() - 1) == 3) {
+                return THREE_KIND_SCORE;
+            } else {
+                return TWO_PAIR_SCORE;
+            }
         }
 
-        // high card
-        return 1;
+        if (fingerprint.size() == 4) {
+            return ONE_PAIR_SCORE;
+        }
+
+        return HIGH_CARD_SCORE;
     }
 
     private static List<Bid> getInput() throws Exception {
