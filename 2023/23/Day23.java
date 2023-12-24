55bdc270cf434978a952ab92b7e54fc3985f011d
Andrei Hodanenok <xoda91@gmail.com>
2023-12-24 15:51:56 +0300

    2023: day 23, part 2

--- 2023/23/Day23.java
+++ 2023/23/Day23.java
@@ -14,25 +14,42 @@ import java.util.HashMap;
  */
 public class Day23 {
 
+    // took some inspiration from
+    // https://www.reddit.com/r/adventofcode/comments/18oy4pc/2023_day_23_solutions/
+
     public static void main(String[] args) throws Exception {
         Input map = getInput();
         part1(map);
+        part2(map);
     }
 
     private static void part1(Input map) {
         int longestPath = -1;
 
+        Set<Location> visitedLocations = new HashSet<>();
+        LinkedList<Location> path = new LinkedList<>();
+
         LinkedList<Step> queue = new LinkedList<>();
-        queue.addLast(new Step(0, map.startLocation));
+        queue.addLast(new Step(0, map.startLocation, null));
         while (!queue.isEmpty()) {
             Step currentStep = queue.removeFirst();
+
+            path.addLast(currentStep.location);
+            visitedLocations.add(currentStep.location);
+
             if (currentStep.location.equals(map.endLocation)) {
                 longestPath = Math.max(currentStep.num, longestPath);
+
+                if (!queue.isEmpty()) {
+                    while (!path.getLast().equals(queue.getFirst().prev)) {
+                        visitedLocations.remove(path.removeLast());
+                    }
+                }
+
                 continue;
             }
 
             List<Location> nextLocations = new ArrayList<>();
-
             Slope slope = map.slopes.get(currentStep.location);
             if (slope != null) {
                 if (slope == Slope.UP) {
@@ -53,21 +70,188 @@ public class Day23 {
                 nextLocations.add(currentStep.location.right());
             }
 
+            int count = 0;
             for (Location nextLocation : nextLocations) {
                 if (!map.paths.contains(nextLocation)) {
                     continue;
                 }
 
-                if (!currentStep.visitedPaths.contains(nextLocation)) {
-                    Step step = new Step(currentStep.num + 1, nextLocation);
-                    step.visitedPaths.addAll(currentStep.visitedPaths);
-                    step.visitedPaths.add(nextLocation);
-                    queue.addLast(step);
+                if (visitedLocations.contains(nextLocation)) {
+                    continue;
+                }
+
+                queue.addFirst(new Step(currentStep.num + 1, nextLocation, currentStep.location));
+                count++;
+            }
+
+            if (count == 0 && !queue.isEmpty()) {
+                while (!path.getLast().equals(queue.getFirst().prev)) {
+                    visitedLocations.remove(path.removeLast());
                 }
             }
         }
 
         System.out.println("Part 1: " + longestPath);
+    }
+
+    /* brute-force: ~30 min
+    private static void part2(Input map) {
+        int longestPath = -1;
+
+        Set<Location> visitedLocations = new HashSet<>();
+        LinkedList<Location> path = new LinkedList<>();
+
+        LinkedList<Step> queue = new LinkedList<>();
+        queue.addLast(new Step(0, map.startLocation, null));
+        while (!queue.isEmpty()) {
+            Step currentStep = queue.removeFirst();
+
+            path.addLast(currentStep.location);
+            visitedLocations.add(currentStep.location);
+
+            if (currentStep.location.equals(map.endLocation)) {
+                longestPath = Math.max(currentStep.num, longestPath);
+
+                if (!queue.isEmpty()) {
+                    while (!path.getLast().equals(queue.getFirst().prev)) {
+                        visitedLocations.remove(path.removeLast());
+                    }
+                }
+
+                continue;
+            }
+
+            List<Location> nextLocations = new ArrayList<>();
+            nextLocations.add(currentStep.location.up());
+            nextLocations.add(currentStep.location.down());
+            nextLocations.add(currentStep.location.left());
+            nextLocations.add(currentStep.location.right());
+
+            int count = 0;
+            for (Location nextLocation : nextLocations) {
+                if (!map.paths.contains(nextLocation)) {
+                    continue;
+                }
+
+                if (visitedLocations.contains(nextLocation)) {
+                    continue;
+                }
+
+                queue.addFirst(new Step(currentStep.num + 1, nextLocation, currentStep.location));
+                count++;
+            }
+
+            if (count == 0 && !queue.isEmpty()) {
+                while (!path.getLast().equals(queue.getFirst().prev)) {
+                    visitedLocations.remove(path.removeLast());
+                }
+            }
+        }
+
+        System.out.println("Part 2: " + longestPath);
+    }
+    */
+
+    private static void part2(Input map) {
+        Set<Location> nodes = new HashSet<>();
+        nodes.add(map.startLocation);
+        nodes.add(map.endLocation);
+        for (Location location : map.paths) {
+            int adjacentCount = 0;
+            for (Location nextLocation : List.of(location.up(), location.down(), location.left(), location.right())) {
+                if (map.paths.contains(nextLocation)) {
+                    adjacentCount++;
+                }
+            }
+
+            if (adjacentCount > 2) {
+                nodes.add(location);
+            }
+        }
+
+        TrailGraph graph = new TrailGraph(nodes);
+        for (Location location : nodes) {
+            connectNode(graph, map, location);
+        }
+
+        int longestPath = -1;
+
+        Set<Location> visitedLocations = new HashSet<>();
+        LinkedList<Location> path = new LinkedList<>();
+
+        LinkedList<Step> queue = new LinkedList<>();
+        queue.addLast(new Step(0, map.startLocation, null));
+        while (!queue.isEmpty()) {
+            Step currentStep = queue.removeFirst();
+
+            path.addLast(currentStep.location);
+            visitedLocations.add(currentStep.location);
+
+            if (currentStep.location.equals(map.endLocation)) {
+                longestPath = Math.max(currentStep.num, longestPath);
+
+                if (!queue.isEmpty()) {
+                    while (!path.getLast().equals(queue.getFirst().prev)) {
+                        visitedLocations.remove(path.removeLast());
+                    }
+                }
+
+                continue;
+            }
+
+            int count = 0;
+            for (Location nextLocation : graph.adjacentTo(currentStep.location)) {
+                if (visitedLocations.contains(nextLocation)) {
+                    continue;
+                }
+
+                queue.addFirst(new Step(
+                    currentStep.num + graph.getDistance(currentStep.location, nextLocation),
+                    nextLocation,
+                    currentStep.location));
+                count++;
+            }
+
+            if (count == 0 && !queue.isEmpty()) {
+                while (!path.getLast().equals(queue.getFirst().prev)) {
+                    visitedLocations.remove(path.removeLast());
+                }
+            }
+        }
+
+        System.out.println("Part 2: " + longestPath);
+    }
+
+    private static void connectNode(TrailGraph graph, Input map, Location fromNode) {
+        Set<Location> visitedLocations = new HashSet<>();
+
+        LinkedList<Step> queue = new LinkedList<>();
+        queue.addLast(new Step(0, fromNode, null));
+        while (!queue.isEmpty()) {
+            Step currentStep = queue.removeFirst();
+            visitedLocations.add(currentStep.location);
+
+            if (!currentStep.location.equals(fromNode) && graph.contains(currentStep.location)) {
+                graph.connect(fromNode, currentStep.location, currentStep.num);
+                continue;
+            }
+
+            for (Location nextLocation : List.of(
+                    currentStep.location.up(),
+                    currentStep.location.down(),
+                    currentStep.location.left(),
+                    currentStep.location.right())) {
+                if (!map.paths.contains(nextLocation)) {
+                    continue;
+                }
+
+                if (visitedLocations.contains(nextLocation)) {
+                    continue;
+                }
+
+                queue.addLast(new Step(currentStep.num + 1, nextLocation, null));
+            }
+        }
     }
 
     private static Input getInput() throws Exception {
@@ -133,11 +317,40 @@ public class Day23 {
 
         final int num;
         final Location location;
-        final Set<Location> visitedPaths = new HashSet<>();
+        final Location prev;
 
-        Step(int num, Location location) {
+        Step(int num, Location location, Location prev) {
             this.num = num;
             this.location = location;
+            this.prev = prev;
+        }
+    }
+
+    private static class TrailGraph {
+
+        private final Map<Location, Map<Location, Integer>> nodes = new HashMap<>();
+
+        TrailGraph(Set<Location> nodes) {
+            for (Location location : nodes) {
+                this.nodes.put(location, new HashMap<>());
+            }
+        }
+
+        boolean contains(Location node) {
+            return nodes.containsKey(node);
+        }
+
+        void connect(Location fromNode, Location toNode, int distance) {
+            nodes.get(fromNode).put(toNode, distance);
+            nodes.get(toNode).put(fromNode, distance);
+        }
+
+        Set<Location> adjacentTo(Location node) {
+            return nodes.get(node).keySet();
+        }
+
+        int getDistance(Location fromNode, Location toNode) {
+            return nodes.get(fromNode).get(toNode);
         }
     }
 
