a3eeff58f26ab259c23e28156a79ba0d11c5e73c
Andrei Hodanenok <xoda91@gmail.com>
2024-01-01 12:41:03 +0300

    2023: day 22, part 2

--- 2023/22/Day22.java
+++ 2023/22/Day22.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.IdentityHashMap;
+import java.util.stream.Collectors;
 
 /**
  * Advent of Code - Day 22
@@ -15,41 +16,23 @@ public class Day22 {
     public static void main(String[] args) throws Exception {
         List<Brick> bricks = getInput();
         part1(bricks);
+        part2(bricks);
     }
 
     private static void part1(List<Brick> bricks) {
-        TreeMap<Integer, List<Brick>> bricksByZ = new TreeMap<>();
-        for (Brick brick : bricks) {
-            bricksByZ.computeIfAbsent(
-                Math.min(brick.startPos.z, brick.endPos.z), __ -> new ArrayList<>()).add(brick);
-        }
-
-        List<Brick> fallenBricks = new ArrayList<>();
-        for (Map.Entry<Integer, List<Brick>> entry : bricksByZ.entrySet()) {
-            for (Brick fallingBrick : entry.getValue()) {
-                int targetZ = 1;
-                for (Brick stillBrick : fallenBricks) {
-                    int z = Math.max(stillBrick.startPos.z, stillBrick.endPos.z) + 1;
-                    if (fallingBrick.intersectXY(stillBrick) && z > targetZ) {
-                        targetZ = z;
-                    }
-                }
-
-                fallenBricks.add(fallingBrick.moveZ(targetZ));
-            }
-        }
+        BricksHeap heap = fall(bricks);
 
         Map<Brick, List<Brick>> supportAboveByBrick = new IdentityHashMap<>();
         Map<Brick, List<Brick>> supportBelowByBrick = new IdentityHashMap<>();
-        for (int i = 0; i < fallenBricks.size(); i++) {
-            Brick a = fallenBricks.get(i);
+        for (int i = 0; i < heap.bricks.size(); i++) {
+            Brick a = heap.bricks.get(i);
             List<Brick> supportedBricks = new ArrayList<>();
-            for (int j = 0; j < fallenBricks.size(); j++) {
+            for (int j = 0; j < heap.bricks.size(); j++) {
                 if (i == j) {
                     continue;
                 }
 
-                Brick b = fallenBricks.get(j);
+                Brick b = heap.bricks.get(j);
                 if (a.adjacentZ(b) && a.intersectXY(b)) {
                     supportedBricks.add(b);
                 }
@@ -62,7 +45,7 @@ public class Day22 {
         }
 
         int count = 0;
-        for (Brick brick : fallenBricks) {
+        for (Brick brick : heap.bricks) {
             List<Brick> supportedBricks = supportAboveByBrick.get(brick);
             if (supportedBricks.isEmpty()) {
                 count++;
@@ -84,10 +67,56 @@ public class Day22 {
         System.out.println("Part 1: " + count);
     }
 
+    private static void part2(List<Brick> bricks) {
+        int count = 0;
+        for (Brick brick : bricks) {
+            BricksHeap heapBefore = fall(bricks);
+            BricksHeap heapAfter = fall(
+                heapBefore.bricks.stream()
+                    .filter(b -> b.id != brick.id)
+                    .collect(Collectors.toList()));
+            count += heapAfter.fallenCount;
+        }
+
+        System.out.println("Part 2: " + count);
+    }
+
+    private static BricksHeap fall(List<Brick> bricks) {
+        Map<Integer, List<Brick>> bricksByZ = new TreeMap<>();
+        for (Brick brick : bricks) {
+            bricksByZ.computeIfAbsent(
+                Math.min(brick.startPos.z, brick.endPos.z), __ -> new ArrayList<>()).add(brick);
+        }
+
+        int fallenCount = 0;
+        List<Brick> fallenBricks = new ArrayList<>();
+        for (Map.Entry<Integer, List<Brick>> entry : bricksByZ.entrySet()) {
+            for (Brick fallingBrick : entry.getValue()) {
+                int targetZ = 1;
+                for (Brick stillBrick : fallenBricks) {
+                    int z = Math.max(stillBrick.startPos.z, stillBrick.endPos.z) + 1;
+                    if (fallingBrick.intersectXY(stillBrick) && z > targetZ) {
+                        targetZ = z;
+                    }
+                }
+
+                if (targetZ != Math.min(fallingBrick.startPos.z, fallingBrick.endPos.z)) {
+                    fallenBricks.add(fallingBrick.moveZ(targetZ));
+                    fallenCount++;
+                } else {
+                    fallenBricks.add(fallingBrick);
+                }
+            }
+        }
+
+        return new BricksHeap(fallenBricks, fallenCount);
+    }
+
     private static List<Brick> getInput() throws Exception {
         try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
             List<Brick> bricks = new ArrayList<>();
 
+            int id = 1;
             String line;
             while ((line = reader.readLine()) != null) {
                 line = line.trim();
@@ -95,6 +124,7 @@ public class Day22 {
                 String[] startParts = parts[0].trim().split(",");
                 String[] endParts = parts[1].trim().split(",");
                 bricks.add(new Brick(
+                    id++,
                     new Position(
                         Integer.parseInt(startParts[0].trim()),
                         Integer.parseInt(startParts[1].trim()),
@@ -109,12 +139,25 @@ public class Day22 {
         }
     }
 
+    private static class BricksHeap  {
+
+        final int fallenCount;
+        final List<Brick> bricks;
+
+        BricksHeap(List<Brick> bricks, int fallenCount) {
+            this.bricks = bricks;
+            this.fallenCount = fallenCount;
+        }
+    }
+
     private static class Brick {
 
+        final int id;
         final Position startPos;
         final Position endPos;
 
-        Brick(Position startPos, Position endPos) {
+        Brick(int id, Position startPos, Position endPos) {
+            this.id = id;
             this.startPos = startPos;
             this.endPos = endPos;
         }
@@ -133,6 +176,7 @@ public class Day22 {
 
         Brick moveZ(int toZ) {
             return new Brick(
+                id,
                 new Position(startPos.x, startPos.y, toZ),
                 new Position(endPos.x, endPos.y, toZ + Math.abs(startPos.z - endPos.z)));
         }
