8cf371a7f36f9b244ef3dd37f5bc7c66c799989f
Andrei Hodanenok <xoda91@gmail.com>
2024-12-04 21:08:13 +0300

    2024: day 3, part 2

--- 2024/03/Day3.java
+++ 2024/03/Day3.java
@@ -12,6 +12,7 @@ public class Day3 {
     public static void main(String... args) throws Exception {
         String memory = getInput();
         part1(memory);
+        part2(memory);
     }
 
     private static void part1(String memory) {
@@ -24,6 +25,26 @@ public class Day3 {
         }
 
         System.out.println("Part 1: " + result);
+    }
+
+    private static void part2(String memory) {
+        long result = 0;
+
+        boolean enabled = true;
+        Pattern grammar = Pattern.compile("do\\(\\)|don't\\(\\)|mul\\((\\d+),(\\d+)\\)");
+        Matcher matcher = grammar.matcher(memory);
+        while (matcher.find()) {
+            String op = matcher.group(0);
+            if (op.equals("do()")) {
+                enabled = true;
+            } else if (op.equals("don't()")) {
+                enabled = false;
+            } else if (enabled) {
+                result += Long.parseLong(matcher.group(1)) * Long.parseLong(matcher.group(2));
+            }
+        }
+
+        System.out.println("Part 2: " + result);
     }
 
     private static String getInput() throws Exception {
