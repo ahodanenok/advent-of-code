e2a06771040c064a4dfce3c440b32a4ff3ffba92
Andrei Hodanenok <xoda91@gmail.com>
2024-12-15 10:42:33 +0300

    2024: day 14, part 2

--- 2024/14/Day14.java
+++ 2024/14/Day14.java
@@ -11,51 +11,87 @@ import java.util.regex.Pattern;
  */
 public class Day14 {
 
+    private static final int SPACE_HEIGHT = 103;
+    private static final int SPACE_WIDTH = 101;
+
     public static void main(String... args) throws Exception {
         List<Robot> robots = getInput();
         part1(robots);
+        part2(robots);
     }
 
     private static void part1(List<Robot> robots) {
-        int height = 103;
-        int width = 101;
         int seconds = 100;
-        int[][] space = new int[height][width];
+        int[][] space = new int[SPACE_HEIGHT][SPACE_WIDTH];
         for (Robot robot : robots) {
-            int x = (seconds * width + robot.px + seconds * robot.vx) % width;
-            int y = (seconds * height + robot.py + seconds * robot.vy) % height;
+            int x = (seconds * SPACE_WIDTH + robot.px + seconds * robot.vx) % SPACE_WIDTH;
+            int y = (seconds * SPACE_HEIGHT + robot.py + seconds * robot.vy) % SPACE_HEIGHT;
             space[y][x]++;
         }
 
         int sf1 = 0;
-        for (int y = 0; y < height / 2; y++) {
-            for (int x = 0; x < width / 2; x++) {
+        for (int y = 0; y < SPACE_HEIGHT / 2; y++) {
+            for (int x = 0; x < SPACE_WIDTH / 2; x++) {
                 sf1 += space[y][x];
             }
         }
 
         int sf2 = 0;
-        for (int y = height / 2 + 1; y < height; y++) {
-            for (int x = 0; x < width / 2; x++) {
+        for (int y = SPACE_HEIGHT / 2 + 1; y < SPACE_HEIGHT; y++) {
+            for (int x = 0; x < SPACE_WIDTH / 2; x++) {
                 sf2 += space[y][x];
             }
         }
 
         int sf3 = 0;
-        for (int y = 0; y < height / 2; y++) {
-            for (int x = width / 2 + 1; x < width; x++) {
+        for (int y = 0; y < SPACE_HEIGHT / 2; y++) {
+            for (int x = SPACE_WIDTH / 2 + 1; x < SPACE_WIDTH; x++) {
                 sf3 += space[y][x];
             }
         }
 
         int sf4 = 0;
-        for (int y = height / 2 + 1; y < height; y++) {
-            for (int x = width / 2 + 1; x < width; x++) {
+        for (int y = SPACE_HEIGHT / 2 + 1; y < SPACE_HEIGHT; y++) {
+            for (int x = SPACE_WIDTH / 2 + 1; x < SPACE_WIDTH; x++) {
                 sf4 += space[y][x];
             }
         }
 
         System.out.println("Part 1: " + (sf1 * sf2 * sf3 * sf4));
+    }
+
+    private static void part2(List<Robot> robots) throws Exception {
+        int seconds = 0;
+        int[][] space = new int[SPACE_HEIGHT][SPACE_WIDTH];
+        for (Robot robot : robots) {
+            space[robot.py][robot.px]++;
+        }
+
+        while (seconds < 10000) {
+            List<Robot> newRobots = new ArrayList<>();
+            for (Robot robot : robots) {
+                int x = (SPACE_WIDTH + robot.px + robot.vx) % SPACE_WIDTH;
+                int y = (SPACE_HEIGHT + robot.py + robot.vy) % SPACE_HEIGHT;
+                space[y][x]++;
+                space[robot.py][robot.px]--;
+                newRobots.add(new Robot(x, y, robot.vx, robot.vy));
+            }
+            robots = newRobots;
+            seconds++;
+
+            for (int y = 0; y < SPACE_HEIGHT; y++) {
+                for (int x = 0; x < SPACE_WIDTH; x++) {
+                    if (space[y][x] > 0) {
+                        System.out.print('x');
+                    } else {
+                        System.out.print('.');
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println(seconds);
+            System.out.println();
+        }
     }
 
     private static List<Robot> getInput() throws Exception {
