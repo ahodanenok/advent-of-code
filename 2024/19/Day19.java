627155c285f2061c3b537ea98a7bd6f7624628ba
Andrei Hodanenok <xoda91@gmail.com>
2024-12-21 17:30:45 +0300

    2024: day 19, part 2

--- 2024/19/Day19.java
+++ 2024/19/Day19.java
@@ -12,6 +12,7 @@ public class Day19 {
 	public static void main(String... args) throws Exception {
         Input input = getInput();
         part1(input);
+        part2(input);
     }
 
     private static void part1(Input input) {
@@ -23,6 +24,16 @@ public class Day19 {
         }
 
         System.out.println("Part 1: " + possibleCount);
+    }
+
+    private static void part2(Input input) {
+        long waysCount = 0;
+        for (String design : input.designs) {
+            java.util.Map<Integer, Long> cache = new java.util.HashMap<>();
+            waysCount += countWays(design, input.patterns, 0, cache);
+        }
+
+        System.out.println("Part 2: " + waysCount);
     }
 
     private static boolean isPossible(String design, List<String> patterns, int pos) {
@@ -38,6 +49,26 @@ public class Day19 {
         }
 
         return false;
+    }
+
+    private static long countWays(String design, List<String> patterns, int pos, java.util.Map<Integer, Long> cache) {
+        if (pos == design.length()) {
+            return 1;
+        }
+
+        if (cache.containsKey(pos)) {
+            return cache.get(pos);
+        }
+
+        long count = 0;
+        for (String pattern : patterns) {
+            if (design.startsWith(pattern, pos)) {
+                count += countWays(design, patterns, pos + pattern.length(), cache);
+            }
+        }
+        cache.put(pos, count);
+
+        return count;
     }
 
     private static Input getInput() throws Exception {
