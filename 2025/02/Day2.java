94e500ed7ffd81dab9d750da5e8ae782232730f1
Andrei Hodanenok <xoda91@gmail.com>
2025-12-12 22:28:15 +0300

    2025: day 2, part 2

--- 2025/02/Day2.java
+++ 2025/02/Day2.java
@@ -12,6 +12,7 @@ public class Day2 {
     public static void main(String... args) throws Exception {
         List<Range> ranges = getInput();
         part1(ranges);
+        part2(ranges);
     }
 
     private static List<Range> getInput() throws Exception {
@@ -34,14 +35,47 @@ public class Day2 {
         for (Range range : ranges) {
             for (long n = range.start; n <= range.end; n++) {
                 String s = Long.toString(n);
-                int repeatLength = s.length() / 2;
-                if (s.substring(0, repeatLength).equals(s.substring(repeatLength))) {
+                if (isRepeated(s, (s.length() + 1) / 2)) {
                     sum += n;
                 }
             }
         }
 
         System.out.println("Part 1: " + sum);
+    }
+
+    private static void part2(List<Range> ranges) {
+        long sum = 0;
+        for (Range range : ranges) {
+            for (long n = range.start; n <= range.end; n++) {
+                String s = Long.toString(n);
+                for (int repeatLength = 1, halfLength = (s.length() + 1) / 2;
+                        repeatLength <= halfLength;
+                        repeatLength++) {
+                    if (isRepeated(s, repeatLength)) {
+                        sum += n;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Part 2: " + sum);
+    }
+
+    private static boolean isRepeated(String str, int repeatLength) {
+        if (repeatLength == 0 || repeatLength == str.length()) {
+            return false;
+        }
+
+        String pattern = str.substring(0, repeatLength);
+        for (int i = repeatLength, len = str.length(); i < len; i+= repeatLength) {
+            if (!str.substring(i, Math.min(i + repeatLength, len)).equals(pattern)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private static class Range {
